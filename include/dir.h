fa760dadad1ccdaae8bd1449d56fc856ce50cf8e
Shaun Taylor <shaun.taylor@gmail.com>
2012-02-14 22:26:35 -0800

    Added directory structure information to doxygen.

--- include/dir.h
+++ include/dir.h
@@ -1,3 +1,8 @@
+/**
+ * @file dir.h
+ * @brief Directory handling
+ * @ingroup system
+ */
 #ifndef __LIBDRAGON_DIR_H
 #define __LIBDRAGON_DIR_H
 
@@ -5,14 +10,33 @@
 extern "C" {
 #endif
 
-#define DT_REG 1
-#define DT_DIR 2
+/** 
+ * @addtogroup system
+ * @{
+ */
 
+/**
+ * @name Directory entry type definitions
+ * @{
+ */
+/** @brief Regular file */
+#define DT_REG 1
+/** @brief Directory */
+#define DT_DIR 2
+/** @} */
+
+/**
+ * @brief Directory entry structure
+ */
 typedef struct
 {
+    /** @brief The name of the directory entry */
     char d_name[256];
+    /** @brief The type of the directory entry.  See #DT_REG and #DT_DIR. */
     int d_type;
 } dir_t;
+
+/** @} */
 
 int dir_findfirst( const char * const path, dir_t *dir );
 int dir_findnext( const char * const path, dir_t *dir );
