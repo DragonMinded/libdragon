30e729630f9f891630ca4a550722627b7f079de1
Victor Vieux <victorvieux@gmail.com>
2017-09-11 14:32:32 -0700

    fix printf n64tool

--- tools/n64tool.c
+++ tools/n64tool.c
@@ -351,7 +351,7 @@ int main(int argc, char *argv[])
 						
 						if(output_zeros(write_file, num_zeros))
 						{
-							fprintf(stderr, "Invalid offset to seek to!\n", output);
+							fprintf(stderr, "Invalid offset to seek to in %s!\n", output);
 							return -1;
 						}
 						
@@ -438,7 +438,7 @@ int main(int argc, char *argv[])
 		
 		if(output_zeros(write_file, num_zeros))
 		{
-			fprintf(stderr, "Couldn't pad image!\n", output);
+			fprintf(stderr, "Couldn't pad image in %s!\n", output);
 			return -1;
 		}
 		
