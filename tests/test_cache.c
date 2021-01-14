6a55fb07ac7088e90ef102e48d71ce1835a0dd76
Giovanni Bajo <rasky@develer.com>
2021-01-14 19:02:01 +0100

    Initial libdragon testsuite
    This commit adds an initial implementation of libdragon testsuite.
    
    The testsuite creates a ROM that runs a series of tests, that verify
    libdragon functionality and performance. Hopefully, new tests will be
    added as people contribute new functionalities or bugfixes.
    
    The Makefile creates two ROM: one meant for emulators, and another
    for a real N64. The latter also benchmarks execution of all tests
    and fail when the duration of a test changes unexpectedly. This can
    be used to do regression testing on performance improvements contributed
    to the codebase.

---
+++ tests/test_cache.c
@@ -0,0 +1,38 @@
+
+void test_cache_invalidate(TestContext *ctx) {
+	uint8_t buf[128] __attribute__((aligned(16)));
+	const char *dfs_header = "\xde\xad\xbe\xef\xff\xff\xff\xff""DragonFS 2.0\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00";
+	const char *aaa = "\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa";
+
+	for (int i=0;i<32;i++) {
+		for (int j=0;j<32;j++) {
+
+			// Write the whole buffer through cache,
+			// so it's all populated in D-Cache.
+			memset(buf, 0xAA, sizeof(buf));
+
+			// Writeback+Invalidate buf[i..i+j]. Now only
+			// those lines should be invalidated.
+			data_cache_hit_writeback_invalidate(buf+i, j);
+			
+			// Read from ROM (header of DFS)
+			dma_read(UncachedAddr(buf), DFS_DEFAULT_LOCATION, 32);
+			dma_read(UncachedAddr(buf+32), DFS_DEFAULT_LOCATION, 32);
+
+			// For each cache-line, check whether the contents
+			// match what we would expect from it:
+			//  * Invalidated: we should see the data read from DFS
+			//  * Not invalidated: we should see the 0xAA fill in the cache
+			for (int c=0;c<4;c++) {
+				bool should_be_invalidated = (j!=0) && (c >= i/16 && c <= (i+j-1)/16);
+
+				if (should_be_invalidated)
+					ASSERT_EQUAL_MEM(buf+c*16, (uint8_t*)dfs_header+c%2*16, 16,
+						"unexpected data in invalidated cacheline %d (%d/%d)", c, i, j);
+				else
+					ASSERT_EQUAL_MEM(buf+c*16, (uint8_t*)aaa, 16,
+						"unexpected data in not-invalidated cached cacheline %d (%d/%d)", c, i, j);
+			}
+		}
+	}
+}
---
+++ tests/test_dfs.c
@@ -0,0 +1,68 @@
+
+void test_dfs_read(TestContext *ctx) {
+	int fh = dfs_open("counter.dat");
+	ASSERT(fh >= 0, "counter.dat not found");
+	DEFER(dfs_close(fh));
+
+	uint8_t buf[128] __attribute__((aligned(16)));
+
+	// random stress, unaligned buffer
+	for (int i=0;i<256;i++) {
+		uint8_t *ubuf = buf+RANDN(64)+2;
+		int to_read = RANDN(8)+1;
+		int seek = RANDN(8)*256;
+
+		dfs_seek(fh, seek, SEEK_SET);
+		memset(buf, 0xAA, sizeof(buf));
+		dfs_read(ubuf, 1, to_read, fh);
+		ASSERT_EQUAL_MEM(ubuf,
+			(uint8_t*)"\x00\x01\x02\x03\x04\x05\x06\x07",
+			to_read, "invalid unaligned read (%d/%d)", ubuf-buf, to_read);
+		ASSERT_EQUAL_MEM(ubuf+to_read, (uint8_t*)"\xaa\xaa", 2, "unaligned buffer overflow");
+		ASSERT_EQUAL_MEM(ubuf-2, (uint8_t*)"\xaa\xaa", 2, "unaligned buffer underflow");
+	}
+
+	// random stress, aligned buffer
+	for (int i=0;i<256;i++) {
+		uint8_t *ubuf = buf+8+RANDN(4)*8;
+		int to_read = RANDN(3)*8;
+		int seek = RANDN(8)*256;
+
+		dfs_seek(fh, seek, SEEK_SET);
+		memset(buf, 0xAA, sizeof(buf));
+		dfs_read(ubuf, 1, to_read, fh);
+		ASSERT_EQUAL_MEM(ubuf,
+			(uint8_t*)"\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f",
+			to_read, "invalid aligned read (%d/%d)", ubuf-buf, to_read);
+		ASSERT_EQUAL_MEM(ubuf+to_read, (uint8_t*)"\xaa\xaa", 2, "aligned buffer overflow");
+		ASSERT_EQUAL_MEM(ubuf-2, (uint8_t*)"\xaa\xaa", 2, "aligned buffer underflow");
+	}
+
+	uint8_t *abuf = buf+8;
+	memset(buf, 0xAA, sizeof(buf));
+
+	// check subsequent reads
+	dfs_seek(fh, 8, SEEK_SET);
+	dfs_read(abuf, 1, 16, fh);
+	ASSERT_EQUAL_MEM(abuf,
+		(uint8_t*)"\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18",
+		16, "invalid read #2");
+	ASSERT_EQUAL_MEM(abuf+16, (uint8_t*)"\xaa\xaa", 2, "buffer overflow #2");
+	ASSERT_EQUAL_MEM(abuf-2, (uint8_t*)"\xaa\xaa", 2, "buffer underflow #2");
+
+	dfs_read(abuf, 1, 16, fh);
+	ASSERT_EQUAL_MEM(abuf,
+		(uint8_t*)"\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f\x20\x21\x22\x23\x24\x25\x26\x27\x28",
+		16, "invalid read #3");
+	ASSERT_EQUAL_MEM(abuf+16, (uint8_t*)"\xaa\xaa", 2, "buffer overflow #3");
+	ASSERT_EQUAL_MEM(abuf-2, (uint8_t*)"\xaa\xaa", 2, "buffer underflow #3");
+
+	// cross sector boundary
+	dfs_seek(fh, 510, SEEK_SET);
+	dfs_read(abuf, 1, 16, fh);
+	ASSERT_EQUAL_MEM(abuf,
+		(uint8_t*)"\xfe\xff\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d",
+		16, "invalid read #3");
+	ASSERT_EQUAL_MEM(abuf+16, (uint8_t*)"\xaa\xaa", 2, "buffer overflow #3");
+	ASSERT_EQUAL_MEM(abuf-2, (uint8_t*)"\xaa\xaa", 2, "buffer underflow #3");	
+}
