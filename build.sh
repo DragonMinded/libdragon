b4e81dfe02778fd28327e57a71354fd8eb9d8e25
Christopher Bonhage <me@christopherbonhage.com>
2021-02-09 15:02:21 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,30 @@
-#!/bin/bash
-make -j8 clean && \
-make -j8 && \
-make install && \
-make -j8 tools && \
-make tools-install && \
-git clone https://github.com/networkfusion/libmikmod.git /tmp/libmikmod && \
-pushd /tmp/libmikmod/n64 && \
-git checkout 738b1e8b11b470360b1b919680d1d88429d9d174 && \
-make -j8 && \
-make install && \
-popd && \
-rm -rf /tmp/libmikmod && \
-make -j8 examples
+#!/usr/bin/env bash
+
+# Exit this script if any command fails
+set -e
+
+# Limit the number of make jobs to the number of CPUs
+CPU_COUNT=$(nproc)
+
+# Specify where to get libmikmod from and where to put it
+LIBMIKMOD_REPO=https://github.com/networkfusion/libmikmod.git
+LIBMIKMOD_COMMIT=738b1e8b11b470360b1b919680d1d88429d9d174
+LIBMIKMOD_DIR=/tmp/libmikmod
+
+# Clean, build, and install libdragon + tools
+make -j${CPU_COUNT} clobber
+make -j${CPU_COUNT} install tools-install
+
+# Remove the cloned libmikmod repo if it already exists
+[ -d "$LIBMIKMOD_DIR" ] && rm -Rf $LIBMIKMOD_DIR
+# Clone, compile, and install libmikmod
+git clone $LIBMIKMOD_REPO $LIBMIKMOD_DIR
+pushd $LIBMIKMOD_DIR/n64
+git checkout $LIBMIKMOD_COMMIT
+make -j${CPU_COUNT}
+make install
+popd
+rm -Rf $LIBMIKMOD_DIR
+
+# Build all of the libdragon examples
+make -j${CPU_COUNT} examples
