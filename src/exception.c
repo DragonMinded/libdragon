47e538a7e8118b707ca8b4095f931bc7d5a8c443
Ali Naci Erdem <anacierdem@gmail.com>
2021-03-11 21:52:49 +0300

    Minor fixes

--- src/exception.c
+++ src/exception.c
@@ -77,10 +77,6 @@ static void exception_halt() {
 }
 
 void exception_default_handler(exception_t* ex) {
-	// We need to return from the ex handler to gracefully restore interrupt
-	// state so that we can continue using interrupt dependent functionalities.
-	ex->regs->epc = (uint32_t)&exception_halt;
-
 	uint32_t cr = ex->regs->cr;
 	uint32_t sr = ex->regs->sr;
 	uint32_t fcr31 = ex->regs->fc31;
@@ -119,9 +115,9 @@ void exception_default_handler(exception_t* ex) {
 	console_set_render_mode(RENDER_MANUAL);
 	console_clear();
 
-	fprintf(stdout, "%s exception at PC:%08lX\n\n", ex->info, ex->regs->epc);
+	fprintf(stdout, "%s exception at PC:%08lX\n\n", ex->info, ex->regs->epc + (uint32_t)((cr & C0_CAUSE_BD) ? 4 : 0)));
 
-	fprintf(stdout, "CR:%08lX (COP:%1lu BD:%lu)\n", cr, C0_GET_CAUSE_CE(cr), cr & C0_CAUSE_BD);
+	fprintf(stdout, "CR:%08lX (COP:%1lu BD:%lu)\n", cr, C0_GET_CAUSE_CE(cr), (bool)(cr & C0_CAUSE_BD));
 	fprintf(stdout, "SR:%08lX FCR31:%08X BVAdr:%08lX \n", sr, (unsigned int)fcr31, C0_READ_BADVADDR());
 	fprintf(stdout, "----------------------------------------------------------------");
 	fprintf(stdout, "FPU IOP UND OVE DV0 INV NI | INT sw0 sw1 ex0 ex1 ex2 ex3 ex4 tmr");
@@ -202,6 +198,10 @@ void exception_default_handler(exception_t* ex) {
 			fprintf(stdout, "\n");
 		}
 	}
+
+	// We need to return from the ex handler to gracefully restore interrupt
+	// state so that we can continue using interrupt dependent functionalities.
+	ex->regs->epc = (uint32_t)&exception_halt;
 }
 
 /**
--- src/interrupt.c
+++ src/interrupt.c
@@ -594,7 +594,7 @@ void init_interrupts()
         __interrupt_depth = 0;
 
         /* Enable interrupts systemwide. We set the global interrupt enable,
-           and then specifically enable RCP interrupts (INTERRUPT_2). */
+           and then specifically enable RCP interrupts. */
         uint32_t sr = C0_STATUS();
         C0_WRITE_STATUS(sr | C0_STATUS_IE | C0_INTERRUPT_RCP);
     }
