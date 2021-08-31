5de806144aff17505c42f06082b1dd8713a834e7
Giovanni Bajo <rasky@develer.com>
2021-08-31 13:16:30 +0200

    Refactor RSP library.
    This is a refactoring of the existing library. Functions are renamed
    to provide a fixed prefix for namespacing concerns (old names are
    still available and marked as deprecated).
    
    rsp.h now exposes some hardware register definitions so that advanced
    users can play with RSP without having to redefine them. This is
    in line with the trend of letting libdragon also expose more of the
    hardware without keeping it hidden within libraries.
    
    The module is still quite barebone. There is no support for having a
    queue of different ucodes (maybe switched under interrupt?) or doing
    some more advanced synchronization. I think this has to wait further work
    to define how RSP display lists and overlays will work. For now, this
    is just a basic improvement over the existing library.

--- src/rsp.c
+++ src/rsp.c
@@ -7,46 +7,11 @@
 #include "libdragon.h"
 #include "regsinternal.h"
 
-/** @brief SP DMA busy */
-#define SP_STATUS_DMA_BUSY              ( 1 << 2 )
-/** @brief SP IO busy */
-#define SP_STATUS_IO_BUSY               ( 1 << 4 )
-/** @brief SP broke */
-#define SP_STATUS_INTERRUPT_ON_BREAK    ( 1 << 6 )
-/** @brief SP halted */
-#define SP_STATUS_HALTED                ( 1 )
-
-#define SP_STATUS_CLEAR_HALT        0x00001
-#define SP_STATUS_SET_HALT          0x00002
-#define SP_STATUS_CLEAR_BROKE       0x00004
-#define SP_STATUS_CLEAR_INTR        0x00008
-#define SP_STATUS_SET_INTR          0x00010
-#define SP_STATUS_CLEAR_SSTEP       0x00020
-#define SP_STATUS_SET_SSTEP         0x00040
-#define SP_STATUS_CLEAR_INTR_BREAK  0x00080
-#define SP_STATUS_SET_INTR_BREAK    0x00100
-#define SP_STATUS_CLEAR_SIG0        0x00200
-#define SP_STATUS_SET_SIG0          0x00400
-#define SP_STATUS_CLEAR_SIG1        0x00800
-#define SP_STATUS_SET_SIG1          0x01000
-#define SP_STATUS_CLEAR_SIG2        0x02000
-#define SP_STATUS_SET_SIG2          0x04000
-#define SP_STATUS_CLEAR_SIG3        0x08000
-#define SP_STATUS_SET_SIG3          0x10000
-#define SP_STATUS_CLEAR_SIG4        0x20000
-#define SP_STATUS_SET_SIG4          0x40000
-#define SP_STATUS_CLEAR_SIG5        0x80000
-#define SP_STATUS_SET_SIG5          0x100000
-#define SP_STATUS_CLEAR_SIG6        0x200000
-#define SP_STATUS_SET_SIG6          0x400000
-#define SP_STATUS_CLEAR_SIG7        0x800000
-#define SP_STATUS_SET_SIG7          0x1000000
-
-#define SP_DMA_DMEM 0x04000000
-#define SP_DMA_IMEM 0x04001000
-
 /** @brief Static structure to address SP registers */
 static volatile struct SP_regs_s * const SP_regs = (struct SP_regs_s *)0xa4040000;
+
+/** @brief Current ucode being loaded */
+static rsp_ucode_t *cur_ucode = NULL;
 
 /**
  * @brief Wait until the SI is finished with a DMA request
@@ -56,51 +21,63 @@ static void __SP_DMA_wait(void)
     while (SP_regs->status & (SP_STATUS_DMA_BUSY | SP_STATUS_IO_BUSY)) ;
 }
 
-void rsp_init()
+void rsp_init(void)
 {
     /* Make sure RSP is halted */
-    *(volatile uint32_t *) 0xA4080000 = SP_DMA_IMEM;
-    SP_regs->status = SP_STATUS_SET_HALT;
-
-    return;
+    *SP_PC = 0x1000;
+    SP_regs->status = SP_WSTATUS_SET_HALT;
 }
 
-void load_ucode(void* start, unsigned long size)
+void rsp_load(rsp_ucode_t *ucode) {
+    if (cur_ucode != ucode) {
+        rsp_load_code(ucode->code, (uint8_t*)ucode->code_end - ucode->code, 0);
+        rsp_load_data(ucode->data, (uint8_t*)ucode->data_end - ucode->data, 0);
+        cur_ucode = ucode;
+    }
+}
+
+void rsp_load_code(void* start, unsigned long size, unsigned int imem_offset)
 {
+    assert(((uint32_t)start % 8) == 0);
+    assert((imem_offset % 8) == 0);
+
     disable_interrupts();
     __SP_DMA_wait();
 
     SP_regs->DRAM_addr = start;
     MEMORY_BARRIER();
-    SP_regs->RSP_addr = (void*)SP_DMA_IMEM;
+    SP_regs->RSP_addr = SP_IMEM + imem_offset/4;
     MEMORY_BARRIER();
     SP_regs->rsp_read_length = size - 1;
     MEMORY_BARRIER();
 
     __SP_DMA_wait();
     enable_interrupts();
-    return;
 }
 
-void load_data(void* start, unsigned long size)
+void rsp_load_data(void* start, unsigned long size, unsigned int dmem_offset)
 {
+    assert(((uint32_t)start % 8) == 0);
+    assert((dmem_offset % 8) == 0);
+
     disable_interrupts();
     __SP_DMA_wait();
 
     SP_regs->DRAM_addr = start;
     MEMORY_BARRIER();
-    SP_regs->RSP_addr = (void*)SP_DMA_DMEM;
+    SP_regs->RSP_addr = SP_DMEM + dmem_offset/4;
     MEMORY_BARRIER();
     SP_regs->rsp_read_length = size - 1;
     MEMORY_BARRIER();
 
     __SP_DMA_wait();
     enable_interrupts();
-    return;
 }
 
-void read_ucode(void* start, unsigned long size)
+void rsp_read_code(void* start, unsigned long size, unsigned int imem_offset)
 {
+    assert(((uint32_t)start % 8) == 0);
+    assert((imem_offset % 8) == 0);
     data_cache_hit_writeback_invalidate(start, size);
 
     disable_interrupts();
@@ -108,19 +85,20 @@ void read_ucode(void* start, unsigned long size)
 
     SP_regs->DRAM_addr = start;
     MEMORY_BARRIER();
-    SP_regs->RSP_addr = (void*)SP_DMA_IMEM;
+    SP_regs->RSP_addr = SP_IMEM + imem_offset/4;
     MEMORY_BARRIER();
     SP_regs->rsp_write_length = size - 1;
     MEMORY_BARRIER();
     __SP_DMA_wait();
 
     enable_interrupts();
-    return;
 }
 
 
-void read_data(void* start, unsigned long size)
+void rsp_read_data(void* start, unsigned long size, unsigned int dmem_offset)
 {
+    assert(((uint32_t)start % 8) == 0);
+    assert((dmem_offset % 8) == 0);
     data_cache_hit_writeback_invalidate(start, size);
 
     disable_interrupts();
@@ -128,21 +106,30 @@ void read_data(void* start, unsigned long size)
 
     SP_regs->DRAM_addr = start;
     MEMORY_BARRIER();
-    SP_regs->RSP_addr = (void*)SP_DMA_DMEM;
+    SP_regs->RSP_addr = SP_DMEM + dmem_offset/4;
     MEMORY_BARRIER();
     SP_regs->rsp_write_length = size - 1;
     MEMORY_BARRIER();
     __SP_DMA_wait();
 
     enable_interrupts();
-    return;
 }
 
-
-void run_ucode()
+void rsp_run_async(void)
 {
     // set RSP program counter
-    *(volatile uint32_t *) 0xA4080000 = SP_DMA_IMEM;
-    SP_regs->status = SP_STATUS_CLEAR_HALT | SP_STATUS_SET_INTR_BREAK;
-    return;
+    *SP_PC = cur_ucode ? cur_ucode->start_pc : 0;
+    MEMORY_BARRIER();
+    *SP_STATUS = SP_WSTATUS_CLEAR_HALT | SP_WSTATUS_SET_INTR_BREAK;
+}
+
+void rsp_wait(void)
+{
+    while (!(*SP_STATUS & SP_STATUS_HALTED)) {}
+}
+
+void rsp_run(void)
+{
+    rsp_run_async();
+    rsp_wait();
 }
