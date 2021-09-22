00c5ec929327a2254a8a047e24fad7f678e81633
Ali Naci Erdem <anacierdem@gmail.com>
2021-09-22 16:03:04 +0300

    Review changes
        - Enable bash strict mode
        - Also check for Dockerfile diff
        - Simplify some of the docker build parameters
        - Refactor git diff into an action

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
-# Exit this script if any command fails
-set -e
+# Bash strict mode http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+IFS=$'\n\t'
+
+makeWithInclude(){
+  make -j${CPU_COUNT} -I${N64_INST}/include $@
+}
 
 # Limit the number of make jobs to the number of CPUs
 CPU_COUNT=$(nproc)
@@ -12,8 +17,8 @@ LIBMIKMOD_COMMIT=738b1e8b11b470360b1b919680d1d88429d9d174
 LIBMIKMOD_DIR=/tmp/libmikmod
 
 # Clean, build, and install libdragon + tools
-make -j${CPU_COUNT} -I${N64_INST}/include clobber
-make -j${CPU_COUNT} -I${N64_INST}/include install tools-install
+makeWithInclude clobber
+makeWithInclude install tools-install
 
 # Remove the cloned libmikmod repo if it already exists
 [ -d "$LIBMIKMOD_DIR" ] && rm -Rf $LIBMIKMOD_DIR
@@ -21,10 +26,10 @@ make -j${CPU_COUNT} -I${N64_INST}/include install tools-install
 git clone $LIBMIKMOD_REPO $LIBMIKMOD_DIR
 pushd $LIBMIKMOD_DIR/n64
 git checkout $LIBMIKMOD_COMMIT
-make -j${CPU_COUNT} -I${N64_INST}/include
-make -j${CPU_COUNT} -I${N64_INST}/include install
+makeWithInclude
+makeWithInclude install
 popd
 rm -Rf $LIBMIKMOD_DIR
 
 # Build all of the libdragon examples
-make -j${CPU_COUNT} -I${N64_INST}/include examples
+makeWithInclude examples
