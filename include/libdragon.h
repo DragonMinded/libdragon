45b6f9f52ebc61112938eb9f53082a3e4bb6f96f
Giovanni Bajo <rasky@develer.com>
2021-10-31 18:44:13 +0100

    Activate doxygen build on CI
    Upgrade doxygen-public.conf to the latest version (via doxygen -u)
    and turn on warnings as errors, so that the build fails if a PR
    introduces undocumented text.
    
    Also rewrite README.md to modernize it, and make the Jekyll website
    coexist with the doxygen reference. Move the jekyll root to
    website/ so that it's easier to add stuff to it. We will iterate over
    this in the short future.
    
    Fixes #77
    Fixes #198

--- include/libdragon.h
+++ include/libdragon.h
@@ -24,25 +24,6 @@
  * inform libdragon of a nonstandard CIC.
  */
 
-/**
- * @mainpage libdragon
- * @author Shaun Taylor
- *
- * libdragon provides a low level API for all hardware features of the N64. 
- * Currently, it provides basic console support, rudimentary higher level controller functions, 
- * a read only filesystem for appending data to the end of a rom, audio support and some 2D graphics 
- * functionality. It also includes bindings to newlib in order to provide a posix interface 
- * to filesystems.
- *
- * libdragon is currently made up of three core components: the @ref libdragon library, @ref system and
- * @ref dfs.  The core library provides drivers for each hardware component in the N64 and low level API
- * for interacting with the components.  The system hooks provide a translation layer between libdragon
- * and newlib for the purpose of POSIX compatibility.  DragonFS provides the read only filesystem support
- * and is hooked into newlib using the system hooks.
- *
- * Please see the documentation in individual modules for additional information.
- */
-
 /* Easy include wrapper */
 #include "audio.h"
 #include "console.h"
