558ed931b8b9f5323d223a9347ddbc3cea1bc927
Giovanni Bajo <rasky@develer.com>
2021-11-07 10:26:11 +0100

    cop0: add TLB access macros
    Add macros accessing the TLB-related COP0 registers, and the TLB
    opcodes.

--- include/cop0.h
+++ include/cop0.h
@@ -77,7 +77,7 @@
 /** @cond */
 /* Alternative version with different naming */
 #define C0_CR()        C0_CAUSE()
-#define C0_WRITE_CR(x) C0_WRITE_CAUSE()
+#define C0_WRITE_CR(x) C0_WRITE_CAUSE(x)
 /** @endcond */
 
 /**
@@ -109,13 +109,136 @@
 	x; \
 })
 
+/**
+ * @brief Read the COP0 INDEX register
+ * 
+ * This register is used during TLB programming. It holds the index of the TLB
+ * entry being accessed (0-31).
+ */
+#define C0_INDEX() ({ \
+    uint32_t x; \
+    asm volatile("mfc0 %0,$0":"=r"(x)); \
+    x; \
+})
+
+/**
+ * @brief Write the COP0 INDEX register
+ * 
+ * This register is used during TLB programming. It holds the index of the TLB
+ * entry being accessed (0-31).
+ */
+#define C0_WRITE_INDEX(x)    asm volatile("mtc0 %0,$0; nop; nop"::"r"(x))
+
+
+/**
+ * @brief Read the COP0 ENTRYHI register
+ * 
+ * This register is used during TLB programming. It holds the configuration
+ * of the virtual memory entry for the TLB slot being accessed.
+ */
+#define C0_ENTRYHI() ({ \
+    uint32_t x; \
+    asm volatile("mfc0 %0,$10":"=r"(x)); \
+    x; \
+})
+
+/**
+ * @brief Write the COP0 ENTRYHI register
+ * 
+ * This register is used during TLB programming.
+ */
+#define C0_WRITE_ENTRYHI(x)  asm volatile("mtc0 %0,$10; nop; nop"::"r"(x))
+
+/**
+ * @brief Read the COP0 ENTRYLO0 register
+ * 
+ * This register is used during TLB programming. It holds the configuration
+ * of the physical memory entry (even bank) for the TLB slot being accessed.
+ */
+#define C0_ENTRYLO0() ({ \
+    uint32_t x; \
+    asm volatile("mfc0 %0,$2":"=r"(x)); \
+    x; \
+})
+
+/**
+ * @brief Write the COP0 ENTRYLO0 register
+ * 
+ * This register is used during TLB programming. It holds the configuration
+ * of the physical memory entry (even bank) for the TLB slot being accessed.
+ */
+#define C0_WRITE_ENTRYLO0(x) asm volatile("mtc0 %0,$2; nop; nop"::"r"(x))
+
+/**
+ * @brief Read the COP0 ENTRYLO1 register
+ * 
+ * This register is used during TLB programming. It holds the configuration
+ * of the physical memory entry (odd bank) for the TLB slot being accessed.
+ */
+#define C0_ENTRYLO1() ({ \
+    uint32_t x; \
+    asm volatile("mfc0 %0,$3":"=r"(x)); \
+    x; \
+})
+
+/**
+ * @brief Write the COP0 ENTRYLO1 register
+ * 
+ * This register is used during TLB programming. It holds the configuration
+ * of the physical memory entry (even bank) for the TLB slot being accessed.
+ */
+#define C0_WRITE_ENTRYLO1(x) asm volatile("mtc0 %0,$3; nop; nop"::"r"(x))
+
+
+/**
+ * @brief Read the COP0 PAGEMASK register
+ * 
+ * This register is used during TLB programming. It holds the bitmask that
+ * configures the page size of the TLB slot being accessed.
+ */
+#define C0_PAGEMASK() ({ \
+    uint32_t x; \
+    asm volatile("mfc0 %0,$5":"=r"(x)); \
+    x; \
+})
+
+/**
+ * @brief Write the COP0 PAGEMASK register
+ * 
+ * This register is used during TLB programming. It holds the bitmask that
+ * configures the page size of the TLB slot being accessed.
+ */
+#define C0_WRITE_PAGEMASK(x) asm volatile("mtc0 %0,$5; nop; nop"::"r"(x))
+
+
+/**
+ * @brief Read the COP0 WIRED register
+ * 
+ * This register is used during TLB programming. It allows to partition TLB
+ * slots between fixed slots and random slots. The fixed slot pool is the
+ * range [0..WIRED[ and the random pool is the range [WIRED..32[
+ */
+#define C0_WIRED() ({ \
+    uint32_t x; \
+    asm volatile("mfc0 %0,$6":"=r"(x)); \
+    x; \
+})
+
+/**
+ * @brief Write the COP0 WIRED register
+ * 
+ * This register is used during TLB programming. It allows to partition TLB
+ * slots between fixed slots and random slots. The fixed slot pool is the
+ * range [0..WIRED[ and the random pool is the range [WIRED..32[
+ */
+#define C0_WRITE_WIRED(x) asm volatile("mtc0 %0,$6; nop; nop"::"r"(x))
+
 /** @cond */
 /* Deprecated version of macros with wrong naming that include "READ" */
 #define C0_READ_CR()         C0_CAUSE()
 #define C0_READ_EPC()        C0_EPC()
 #define C0_READ_BADVADDR()   C0_BADVADDR()
 /** @endcond */
-
 
 /* COP0 Status bits definition. Please refer to MIPS R4300 manual. */
 #define C0_STATUS_IE        0x00000001      ///< Status: interrupt enable
@@ -153,6 +276,51 @@
  * @brief Get the exception code value from the COP0 status register value
  */
 #define C0_GET_CAUSE_EXC_CODE(sr) (((sr) & C0_CAUSE_EXC_CODE) >> 2)
+
+/* Flag bits valid for COP0 ENTRYLO0/ENTRYLO1 registers */
+#define C0_ENTRYLO_GLOBAL      (1<<0)       ///< ENTRYLO: mapping is global (all ASIDs)
+#define C0_ENTRYLO_VALID       (1<<1)       ///< ENTRYLO: mapping is active (not disabled)
+#define C0_ENTRYLO_DIRTY       (1<<2)       ///< ENTRYLO: mapping is writable
+
+/* Flag bits valid for COP0 INDEX register */
+#define C0_INDEX_PROBE_FAILED  (1<<31)      ///< INDEX: set when a TLBP probe failed to find a match
+
+
+/**
+ * @brief COP0 TLBWI opcode.
+ * 
+ * This opcode is used during TLB programming. It writes the TLB slot referenced
+ * by INDEX with the contents of PAGEMASK, ENTRYHI, ENTRYLO0, ENTRYLO1.
+ */
+#define C0_TLBWI()           asm volatile("tlbwi; nop; nop; nop; nop")
+
+/**
+ * @brief COP0 TLBWR opcode.
+ * 
+ * This opcode is used during TLB programming. It writes a random TLB slot with
+ * the contents of PAGEMASK, ENTRYHI, ENTRYLO0, ENTRYLO1. THe slot is selected
+ * in the random pool (slots in the range from WIRED to 31).
+ */
+#define C0_TLBWR()           asm volatile("tlbwr; nop; nop; nop; nop")
+
+/**
+ * @brief COP0 TLBR opcode.
+ * 
+ * This opcode is used during TLB programming. It reads the contents of the TLB
+ * slot referenced by INDEX into the registers PAGEMASK, ENTRYHI, ENTRYLO0 and
+ * ENTRYLO1.
+ */
+#define C0_TLBR()            asm volatile("tlbr; nop; nop; nop; nop")
+
+/**
+ * @brief COP0 TLBP opcode.
+ * 
+ * This opcode is used during TLB programming. It probes the current TLB slots
+ * using ENTRYHI (virtual address) to find a matching slot. If it finds, it
+ * loads its index into INDEX. Otherwise, it sets the C0_INDEX_PROBE_FAILED bit
+ * in INDEX.
+ */
+#define C0_TLBP()            asm volatile("tlbp; nop; nop; nop; nop")
 
 /** @} */
 
