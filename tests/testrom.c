2ffba5dbc2c2775788f50816f8e771f3a1f9bcc0
Dennis Heinze <dennisjp.heinze@gmail.com>
2021-12-28 15:45:49 +0100

    remove test_rspq_pause since it is not used for now

--- tests/testrom.c
+++ tests/testrom.c
@@ -222,7 +222,7 @@ static const struct Testsuite
 	TEST_FUNC(test_rspq_rapid_sync,            0, TEST_FLAGS_NO_BENCHMARK),
 	TEST_FUNC(test_rspq_block,                 0, TEST_FLAGS_NO_BENCHMARK),
 	TEST_FUNC(test_rspq_wait_sync_in_block,    0, TEST_FLAGS_NO_BENCHMARK),
-	TEST_FUNC(test_rspq_pause,                 0, TEST_FLAGS_NO_BENCHMARK),
+	//TEST_FUNC(test_rspq_pause,                 0, TEST_FLAGS_NO_BENCHMARK),
 	TEST_FUNC(test_rspq_highpri_basic,         0, TEST_FLAGS_NO_BENCHMARK),
 	TEST_FUNC(test_rspq_highpri_multiple,      0, TEST_FLAGS_NO_BENCHMARK),
 	TEST_FUNC(test_ugfx_rdp_interrupt,         0, TEST_FLAGS_NO_BENCHMARK),
