5ac8f9801085b7537e982af265385d3f67226053
Dennis Heinze <dennisjp.heinze@gmail.com>
2022-01-01 16:27:33 +0100

    make rspqdemo more complex

--- examples/rspqdemo/rspqdemo.c
+++ examples/rspqdemo/rspqdemo.c
@@ -1,7 +1,9 @@
 #include "libdragon.h"
+#include <malloc.h>
 
 static wav64_t sfx_cannon;
 static xm64player_t xm;
+static sprite_t *sprite;
 
 typedef struct {
     double r;       // a fraction between 0 and 1
@@ -18,14 +20,108 @@ typedef struct {
 rgb hsv2rgb(hsv in);
 uint32_t rgb16(rgb in);
 
+typedef struct {
+    uint32_t x;
+    uint32_t y;
+    int32_t dx;
+    int32_t dy;
+} object_t;
+
+#define NUM_OBJECTS 64
+
+static object_t objects[NUM_OBJECTS];
+
+// Fair and fast random generation (using xorshift32, with explicit seed)
+static uint32_t rand_state = 1;
+static uint32_t rand(void) {
+	uint32_t x = rand_state;
+	x ^= x << 13;
+	x ^= x >> 7;
+	x ^= x << 5;
+	return rand_state = x;
+}
+
+// RANDN(n): generate a random number from 0 to n-1
+#define RANDN(n) ({ \
+	__builtin_constant_p((n)) ? \
+		(rand()%(n)) : \
+		(uint32_t)(((uint64_t)rand() * (n)) >> 32); \
+})
+
+static uint32_t obj_max_x;
+static uint32_t obj_max_y;
+
+static uint32_t num_objs = 1;
+
+void update(int ovfl)
+{
+    for (uint32_t i = 0; i < NUM_OBJECTS; i++)
+    {
+        object_t *obj = &objects[i];
+        obj->x = (obj->x + obj->dx) % obj_max_x;
+        obj->y = (obj->y + obj->dy) % obj_max_y;
+    }
+}
+
+void render()
+{
+    if (!rdp_can_attach_display())
+    {
+        return;
+    }
+
+    display_context_t disp = display_lock();
+    if (!disp)
+    {
+        return;
+    }
+
+    rdp_attach_display(disp);
+    rdp_set_default_clipping();
+
+    rdp_enable_primitive_fill();
+    
+    double hue = (double)((get_ticks_ms() / 5) % 360);
+    hsv color = { .h = hue, .s = 1.0, .v = 1.0 };
+    uint32_t fill_color = rgb16(hsv2rgb(color));
+    rdp_set_primitive_color(fill_color | (fill_color << 16));
+    
+    uint32_t display_width = display_get_width();
+    uint32_t display_height = display_get_height();
+    rdp_draw_filled_rectangle(0, 0, display_width, display_height);
+
+    rdp_sync_pipe();
+
+    rdp_enable_texture_copy();
+
+    for (uint32_t y = 0; y < sprite->vslices; y++)
+    {
+        for (uint32_t x = 0; x < sprite->hslices; x++)
+        {
+            rdp_sync_load();
+            rdp_load_texture_stride(0, 0, MIRROR_DISABLED, sprite, y*sprite->hslices + x);
+            for (uint32_t i = 0; i < num_objs; i++)
+            {
+                rdp_draw_sprite(0, objects[i].x + x * (sprite->width / sprite->hslices), objects[i].y + y * (sprite->height / sprite->vslices), MIRROR_DISABLED);
+            }
+        }
+    }
+
+    rdp_auto_show_display();
+}
+
 int main()
 {
+    display_init(RESOLUTION_320x240, DEPTH_16_BPP, 3, GAMMA_NONE, ANTIALIAS_RESAMPLE);
+
     debug_init_isviewer();
     debug_init_usblog();
 
     controller_init();
+    timer_init();
 
-    display_init(RESOLUTION_512x240, DEPTH_16_BPP, 3, GAMMA_NONE, ANTIALIAS_RESAMPLE);
+    uint32_t display_width = display_get_width();
+    uint32_t display_height = display_get_height();
     
     dfs_init(DFS_DEFAULT_LOCATION);
 
@@ -33,40 +129,52 @@ int main()
     mixer_init(32);
 
     rdp_init();
+
+    int fp = dfs_open("n64brew.sprite");
+    sprite = malloc(dfs_size(fp));
+    dfs_read(sprite, 1, dfs_size(fp), fp);
+    dfs_close(fp);
+
+    uint32_t obj_min_x = 0;
+    uint32_t obj_min_y = 0;
+    obj_max_x = display_width - sprite->width;
+    obj_max_y = display_height - sprite->height;
+
+    for (uint32_t i = 0; i < NUM_OBJECTS; i++)
+    {
+        object_t *obj = &objects[i];
+
+        obj->x = obj_min_x + RANDN(obj_max_x - obj_min_x);
+        obj->y = obj_min_y + RANDN(obj_max_y - obj_min_y);
+
+        obj->dx = -4 + RANDN(9);
+        obj->dy = -4 + RANDN(9);
+    }
     
     wav64_open(&sfx_cannon, "cannon.wav64");
 
     xm64player_open(&xm, "rom:/Caverns16bit.xm64");
     xm64player_play(&xm, 2);
 
+    new_timer(TIMER_TICKS(1000000 / 30), TF_CONTINUOUS, update);
+
     while (1)
     {
-        display_context_t disp = display_lock();
-        if (disp)
-        {
-            rdp_attach_display(disp);
-            rdp_set_default_clipping();
-
-            uint32_t display_width = display_get_width();
-            uint32_t display_height = display_get_height();
-
-            rdp_enable_primitive_fill();
-            
-            double hue = (double)((get_ticks_ms() / 5) % 360);
-            hsv color = { .h = hue, .s = 1.0, .v = 1.0 };
-            uint32_t fill_color = rgb16(hsv2rgb(color));
-            rdp_set_primitive_color(fill_color | (fill_color << 16));
-            
-            rdp_draw_filled_rectangle(0, 0, display_width, display_height);
-
-            rdp_detach_display_auto_show();
-        }
+        render();
 
         controller_scan();
         struct controller_data ckeys = get_keys_down();
 
         if (ckeys.c[0].A) {
             mixer_ch_play(0, &sfx_cannon.wave);
+        }
+
+        if (ckeys.c[0].C_up && num_objs < NUM_OBJECTS) {
+            ++num_objs;
+        }
+
+        if (ckeys.c[0].C_down && num_objs > 1) {
+            --num_objs;
         }
 
         if (audio_can_write()) {    	
