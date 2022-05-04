5dae1e5f9e6c0f5f8858d84a7418bf4f1acd71a7
Giovanni Bajo <rasky@develer.com>
2022-05-04 12:28:49 +0200

    Implement new interrupt handler on the user-space stack.
    Currently, the interrupt handler uses a dedicated stack of 64K.
    With this change, the handler starts using the standard stack instead,
    without having a reserved one.
    
    Moreover, some cleanup is made to the code to make it easier to edit
    in future and to correlate better to the C code that accesses the
    stack frame.
    
    This comes from the kernel PR:
    https://github.com/DragonMinded/libdragon/pull/151
    
    so that we can do further improvements without conflicting too much.

--- src/exception.c
+++ src/exception.c
@@ -26,7 +26,7 @@
  * @{
  */
 
-/** @brief Exception handler currently registered with exception system */
+/** @brief Unhandled exception handler currently registered with exception system */
 static void (*__exception_handler)(exception_t*) = exception_default_handler;
 /** @brief Base register offset as defined by the interrupt controller */
 extern const void* __baseRegAddr;
@@ -267,10 +267,12 @@ static const char* __get_exception_name(exception_code_t code)
  * @param[in]  type
  *             Exception type.  Either #EXCEPTION_TYPE_CRITICAL or 
  *             #EXCEPTION_TYPE_RESET
+ * @param[in]  regs
+ *             CPU register status at exception time
  */
-static void __fetch_regs(exception_t* e,int32_t type)
+static void __fetch_regs(exception_t* e, int32_t type, volatile reg_block_t *regs)
 {
-	e->regs = (volatile reg_block_t*) &__baseRegAddr;
+	e->regs = regs;
 	e->type = type;
 	e->code = C0_GET_CAUSE_EXC_CODE(e->regs->cr);
 	e->info = __get_exception_name(e->code);
@@ -279,26 +281,26 @@ static void __fetch_regs(exception_t* e,int32_t type)
 /**
  * @brief Respond to a critical exception
  */
-void __onCriticalException()
+void __onCriticalException(volatile reg_block_t* regs)
 {
 	exception_t e;
 
 	if(!__exception_handler) { return; }
 
-	__fetch_regs(&e,EXCEPTION_TYPE_CRITICAL);
+	__fetch_regs(&e, EXCEPTION_TYPE_CRITICAL, regs);
 	__exception_handler(&e);
 }
 
 /**
  * @brief Respond to a reset exception
  */
-void __onResetException()
+void __onResetException(volatile reg_block_t* regs)
 {
 	exception_t e;
 	
 	if(!__exception_handler) { return; }
 
-	__fetch_regs(&e,EXCEPTION_TYPE_RESET);
+	__fetch_regs(&e, EXCEPTION_TYPE_RESET, regs);
 	__exception_handler(&e);
 }
 
