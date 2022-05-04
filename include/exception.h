9c8d59dbe8a0ed20fc306e537d178f36f83f4b2b
Giovanni Bajo <rasky@develer.com>
2022-05-04 12:28:49 +0200

    Implement new interrupt handler on the user-space stack.
    Currently, the interrupt handler uses a dedicated stack of 64K.
    With this change, the handler starts using the standard stack instead,
    without having a reserved one.
    
    Moreover, some cleanup is made to the code to make it easier to edit
    in future and to correlate better to the C code that accesses the
    stack frame.
    
    This comes from the kernel PR:
    https://github.com/DragonMinded/libdragon/pull/151
    
    so that we can do further improvements without conflicting too much.

--- include/exception.h
+++ include/exception.h
@@ -19,33 +19,33 @@
 enum
 {
     /** @brief Unknown exception */
-	EXCEPTION_TYPE_UNKNOWN = 0,
+    EXCEPTION_TYPE_UNKNOWN = 0,
     /** @brief Reset exception */
-	EXCEPTION_TYPE_RESET,
+    EXCEPTION_TYPE_RESET,
     /** @brief Critical exception */
-	EXCEPTION_TYPE_CRITICAL
+    EXCEPTION_TYPE_CRITICAL
 };
 
 /**
  * @brief Exception codes
  */
 typedef enum {
-	EXCEPTION_CODE_INTERRUPT = 0,
-	EXCEPTION_CODE_TLB_MODIFICATION = 1,
-	EXCEPTION_CODE_TLB_LOAD_I_MISS = 2,
-	EXCEPTION_CODE_TLB_STORE_MISS = 3,
-	EXCEPTION_CODE_LOAD_I_ADDRESS_ERROR = 4,
-	EXCEPTION_CODE_STORE_ADDRESS_ERROR = 5,
-	EXCEPTION_CODE_I_BUS_ERROR = 6,
-	EXCEPTION_CODE_D_BUS_ERROR = 7,
-	EXCEPTION_CODE_SYS_CALL = 8,
-	EXCEPTION_CODE_BREAKPOINT = 9,
-	EXCEPTION_CODE_RESERVED_INSTRUCTION = 10,
-	EXCEPTION_CODE_COPROCESSOR_UNUSABLE = 11,
-	EXCEPTION_CODE_ARITHMETIC_OVERFLOW = 12,
-	EXCEPTION_CODE_TRAP = 13,
-	EXCEPTION_CODE_FLOATING_POINT = 15,
-	EXCEPTION_CODE_WATCH = 23,
+    EXCEPTION_CODE_INTERRUPT = 0,
+    EXCEPTION_CODE_TLB_MODIFICATION = 1,
+    EXCEPTION_CODE_TLB_LOAD_I_MISS = 2,
+    EXCEPTION_CODE_TLB_STORE_MISS = 3,
+    EXCEPTION_CODE_LOAD_I_ADDRESS_ERROR = 4,
+    EXCEPTION_CODE_STORE_ADDRESS_ERROR = 5,
+    EXCEPTION_CODE_I_BUS_ERROR = 6,
+    EXCEPTION_CODE_D_BUS_ERROR = 7,
+    EXCEPTION_CODE_SYS_CALL = 8,
+    EXCEPTION_CODE_BREAKPOINT = 9,
+    EXCEPTION_CODE_RESERVED_INSTRUCTION = 10,
+    EXCEPTION_CODE_COPROCESSOR_UNUSABLE = 11,
+    EXCEPTION_CODE_ARITHMETIC_OVERFLOW = 12,
+    EXCEPTION_CODE_TRAP = 13,
+    EXCEPTION_CODE_FLOATING_POINT = 15,
+    EXCEPTION_CODE_WATCH = 23,
 } exception_code_t;
 
 /**
@@ -53,34 +53,37 @@ typedef enum {
  *
  * DO NOT modify the order unless editing inthandler.S
  */
-typedef volatile struct
+typedef struct __attribute__((packed))
 {
     /** @brief General purpose registers 1-32 */
-	volatile uint64_t gpr[32];
-    /** @brief SR */
-	volatile uint32_t sr;
-    /** @brief CR */
-	volatile const uint32_t cr;
-    /**
-	 * @brief represents EPC - COP0 register $14
-	 *
-	 * The coprocessor 0 (system control coprocessor - COP0) register $14 is the
-	 * return from exception program counter. For asynchronous exceptions it points
-	 * to the place to continue execution whereas for synchronous (caused by code)
-	 * exceptions, point to the instruction causing the fault condition, which
-	 * needs correction in the exception handler. This member is for reading/writing
-	 * its value.
-	 * */
-	volatile uint32_t epc;
+    uint64_t gpr[32];
     /** @brief HI */
-	volatile uint64_t hi;
+    uint64_t hi;
     /** @brief LO */
-	volatile uint64_t lo;
+    uint64_t lo;
+    /** @brief SR */
+    uint32_t sr;
+    /** @brief CR (NOTE: can't modify this from an exception handler) */
+    uint32_t cr;
+    /**
+     * @brief represents EPC - COP0 register $14
+     *
+     * The coprocessor 0 (system control coprocessor - COP0) register $14 is the
+     * return from exception program counter. For asynchronous exceptions it points
+     * to the place to continue execution whereas for synchronous (caused by code)
+     * exceptions, point to the instruction causing the fault condition, which
+     * needs correction in the exception handler. This member is for reading/writing
+     * its value.
+     * */
+    uint32_t epc;
     /** @brief FC31 */
-	volatile uint32_t fc31;
+    uint32_t fc31;
     /** @brief Floating point registers 1-32 */
-	volatile uint64_t fpr[32];
+    uint64_t fpr[32];
 } reg_block_t;
+
+/* Make sure the structure has the right size. Please keep this in sync with inthandler.S */
+_Static_assert(sizeof(reg_block_t) == 544, "invalid reg_block_t size -- this must match inthandler.S");
 
 /**
  * @brief Structure representing an exception
@@ -91,15 +94,15 @@ typedef struct
      * @brief Exception type
      * @see #EXCEPTION_TYPE_RESET, #EXCEPTION_TYPE_CRITICAL
      */
-	int32_t type;
-	/**
-	 * @brief Underlying exception code
-	 */
-	exception_code_t code;
+    int32_t type;
+    /**
+     * @brief Underlying exception code
+     */
+    exception_code_t code;
     /** @brief String information of exception */
-	const char* info;
+    const char* info;
     /** @brief Registers at point of exception */
-	volatile reg_block_t* regs;
+    volatile reg_block_t* regs;
 } exception_t;
 
 /** @} */
