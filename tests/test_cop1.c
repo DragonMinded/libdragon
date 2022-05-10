5f58ed7aa710739b46ba61c79f72528269bd2ac9
Giovanni Bajo <rasky@develer.com>
2022-05-10 18:12:00 +0200

    interrupt: disable FPU access by default under interrupt
    To speed up interrupt handling, we disable FPU by default in interrupt
    handlers. This saves 160 bytes of stack saving/restoring (20 FPU
    registers).
    
    When a FPU instruction is emitted under interrupt, a nested exception
    triggers, FPU access is restored and registers are then saved on the
    stacks. So interrupt callbacks which use floating points still work as
    before, though with a little bit of additional cost for an additional
    exception. All other interrupts (hopefully, the vast majority) become
    much faster and waste fewer cachelines. This should be a net win.

--- tests/test_cop1.c
+++ tests/test_cop1.c
@@ -11,3 +11,31 @@ void test_cop1_denormalized_float(TestContext *ctx) {
        "not implemented" exception was not raised */
     ASSERT(x == 0.0f, "Denormalized float was not flushed to zero");
 }
+
+void test_cop1_interrupts(TestContext *ctx) {
+   // Test that we can use FPUs in the context of an interrupt handler.
+   // This is useful because in general interrupt handlers save FPU registers
+   // only "on demand" when needed.
+   timer_init();
+   DEFER(timer_close());
+
+   volatile float float_value = 1234.0f;
+   void cb1(int ovlf) {
+      disable_interrupts();
+      float_value *= 2;
+      enable_interrupts();
+   }
+
+   void cb2(int ovlf) {
+      float_value *= 2;
+   }
+
+   timer_link_t *tt1 = new_timer(TICKS_FROM_MS(2), TF_ONE_SHOT, cb1);
+   DEFER(delete_timer(tt1));
+   timer_link_t *tt2 = new_timer(TICKS_FROM_MS(2), TF_ONE_SHOT, cb2);
+   DEFER(delete_timer(tt2));
+
+   wait_ms(3);
+
+   ASSERT_EQUAL_SIGNED((int)float_value, 4936, "invalid floating point value");
+}
