c792613156d7d509e0d3fccdb9e9efb6fbfd3e5c
Giovanni Bajo <rasky@develer.com>
2022-08-02 00:50:45 +0200

    Add missing test

--- tests/testrom.c
+++ tests/testrom.c
@@ -244,6 +244,7 @@ static const struct Testsuite
 	TEST_FUNC(test_rdpq_fixup_texturerect,     0, TEST_FLAGS_NO_BENCHMARK),
 	TEST_FUNC(test_rdpq_lookup_address,        0, TEST_FLAGS_NO_BENCHMARK),
 	TEST_FUNC(test_rdpq_lookup_address_offset, 0, TEST_FLAGS_NO_BENCHMARK),
+	TEST_FUNC(test_rdpq_syncfull,              0, TEST_FLAGS_NO_BENCHMARK),
 	TEST_FUNC(test_rdpq_autosync,              0, TEST_FLAGS_NO_BENCHMARK),
 	TEST_FUNC(test_rdpq_automode,              0, TEST_FLAGS_NO_BENCHMARK),
 };
