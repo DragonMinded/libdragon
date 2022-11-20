4b395e97940b6b90153a744e601c926352366448
rasky <rasky@users.noreply.github.com>
2022-11-20 10:11:13 +0000

    deploy: 5af26ad085738475c00e72c2d3a067f357be6171

--- ref/search/variables_0.js
+++ ref/search/variables_0.js
@@ -6,10 +6,11 @@ var searchData=
   ['_5f_5fbootcic_3',['__bootcic',['../group__n64sys.html#ga7bde5bc5b3121ab6d4d1293f29a2718b',1,'__bootcic():&#160;n64sys.c'],['../group__system.html#ga7bde5bc5b3121ab6d4d1293f29a2718b',1,'__bootcic():&#160;n64sys.c']]],
   ['_5f_5fctor_5fend_5f_5f_4',['__CTOR_END__',['../group__system.html#ga537f421bb42d6566f23aabcdcc0aa0f3',1,'do_ctors.c']]],
   ['_5f_5fctor_5flist_5f_5f_5',['__CTOR_LIST__',['../group__system.html#ga85d9cd4c96818595113f38830217f2f9',1,'do_ctors.c']]],
-  ['_5f_5fenv_6',['__env',['../group__system.html#ga5428d66981cb84e60b899495874618b2',1,'system.c']]],
-  ['_5f_5fmixer_5fprofile_5frsp_7',['__mixer_profile_rsp',['../mixer_8c.html#adb6462ba394e665c815abb852d8e341f',1,'mixer.c']]],
-  ['_5f_5fpad0_5f_5f_8',['__pad0__',['../group__controller.html#a9d55f6a808529317eea1a9c6e6a2d203',1,'_SI_condat']]],
-  ['_5f_5fpad1_5f_5f_9',['__pad1__',['../group__controller.html#ae1549b09333a3d1e0ee817606319e15f',1,'_SI_condat']]],
-  ['_5f_5from_5fend_10',['__rom_end',['../group__n64sys.html#ga4e07cc711f6f6d1853ee441b43cb54cb',1,'n64sys.h']]],
-  ['_5f_5fwav64_5fprofile_5fdma_11',['__wav64_profile_dma',['../wav64_8c.html#a6090ed2c8a2f8e13225458264164e11a',1,'wav64.c']]]
+  ['_5f_5feh_5fframe_5fbegin_5f_5f_6',['__EH_FRAME_BEGIN__',['../group__system.html#ga9efca5b23bc6ea3165cab5b4ebfb4c77',1,'do_ctors.c']]],
+  ['_5f_5fenv_7',['__env',['../group__system.html#ga5428d66981cb84e60b899495874618b2',1,'system.c']]],
+  ['_5f_5fmixer_5fprofile_5frsp_8',['__mixer_profile_rsp',['../mixer_8c.html#adb6462ba394e665c815abb852d8e341f',1,'mixer.c']]],
+  ['_5f_5fpad0_5f_5f_9',['__pad0__',['../group__controller.html#a9d55f6a808529317eea1a9c6e6a2d203',1,'_SI_condat']]],
+  ['_5f_5fpad1_5f_5f_10',['__pad1__',['../group__controller.html#ae1549b09333a3d1e0ee817606319e15f',1,'_SI_condat']]],
+  ['_5f_5from_5fend_11',['__rom_end',['../group__n64sys.html#ga4e07cc711f6f6d1853ee441b43cb54cb',1,'n64sys.h']]],
+  ['_5f_5fwav64_5fprofile_5fdma_12',['__wav64_profile_dma',['../wav64_8c.html#a6090ed2c8a2f8e13225458264164e11a',1,'wav64.c']]]
 ];
