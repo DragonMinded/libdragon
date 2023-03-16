f522414cada9d3d348f06628ea4df36403872ea8
gamemasterplc <gamemasterplc@gmail.com>
2023-03-16 10:40:38 -0500

    Implement module linked list

--- src/dlfcn.c
+++ src/dlfcn.c
@@ -18,10 +18,43 @@ _Static_assert(sizeof(uso_section_t) == 28, "uso_section_t size is wrong");
 _Static_assert(sizeof(uso_module_t) == 32, "uso_module_t size is wrong");
 _Static_assert(sizeof(uso_load_info_t) == 12, "uso_load_info_t size is wrong");
 
-/** @brief USO list head */
-struct dl_module_s *__dl_module_head;
-/** @brief USO list tail */
-struct dl_module_s *__dl_module_tail;
+/** @brief Module list head */
+static dl_module_t *module_list_head;
+/** @brief Module list tail */
+static dl_module_t *module_list_tail;
+
+static void insert_module(dl_module_t *module)
+{
+    dl_module_t *prev = module_list_tail;
+    //Insert module at end of list
+    if(!prev) {
+        module_list_head = module;
+    } else {
+        prev->next = module;
+    }
+    //Set up module links
+    module->prev = prev;
+    module->next = NULL;
+    module_list_tail = module; //Mark this module as end of list
+}
+
+static void remove_module(dl_module_t *module)
+{
+    dl_module_t *next = module->next;
+    dl_module_t *prev = module->prev;
+    //Remove back links to this module
+    if(!next) {
+        module_list_tail = prev;
+    } else {
+        next->prev = prev;
+    }
+    //Remove forward links to this module
+    if(!prev) {
+        module_list_head = next;
+    } else {
+        prev->next = next;
+    }
+}
 
 void *dlopen(const char *path, int flags)
 {
@@ -43,12 +76,36 @@ int dladdr(const void *addr, Dl_info *sym_info)
     return 1;
 }
 
-struct dl_module_s *__dl_get_loaded_module(void *addr)
+char *dlerror(void)
 {
     return NULL;
 }
 
-char *dlerror(void)
+dl_module_t *__dl_get_module(void *addr)
 {
     return NULL;
+}
+
+size_t __dl_get_num_modules()
+{
+    dl_module_t *curr = module_list_head;
+    size_t num_modules = 0;
+    while(curr) {
+        curr = curr->next;
+        num_modules++;
+    }
+    return num_modules;
+}
+
+dl_module_t *__dl_get_first_module()
+{
+    return module_list_head;
+}
+
+dl_module_t *__dl_get_next_module(dl_module_t *module)
+{
+    if(!module) {
+        return NULL;
+    }
+    return module->next;
 }
--- src/dlfcn_internal.h
+++ src/dlfcn_internal.h
@@ -5,26 +5,47 @@
 #include "uso_format.h"
 
 /** @brief Loaded module data */
-struct dl_module_s {
+typedef struct dl_module_s {
     struct dl_module_s *prev;   ///< Previous loaded dynamic library
     struct dl_module_s *next;   ///< Next loaded dynamic library
     uso_module_t *module;       ///< USO file
+    size_t module_size;         ///< USO size
     uint32_t debugsym_romaddr;  ///< Debug symbol data rom address
     char *path;                 ///< Dynamic library path
     size_t ref_count;           ///< Dynamic library reference count
     uint32_t ehframe_obj[6];    ///< Exception frame object
     int flags;                  ///< Dynamic library flags
-};
-
-extern struct dl_module_s *__dl_module_head;
-extern struct dl_module_s *__dl_module_tail;
+} dl_module_t;
 
 /**
- * @brief Get Loaded module from address
+ * @brief Get pointer to loaded module from address
  * 
- * @param addr                  Address to search
- * @return struct dl_module_s*  Pointer to module address is found inside
+ * @param addr          Address to search
+ * @return dl_module_t* Pointer to module address is found inside
  */
-struct dl_module_s *__dl_get_loaded_module(void *addr);
+dl_module_t *__dl_get_module(void *addr);
+
+/**
+ * @brief Get number of loaded modules
+ * 
+ * @return size_t   Number of loaded modules
+ */
+size_t __dl_get_num_modules();
+
+/**
+ * @brief Get first loaded module
+ * 
+ * @return dl_module_t*  Pointer to first module
+ */
+dl_module_t *__dl_get_first_module();
+
+
+/**
+ * @brief Get next loaded module
+ * 
+ * @param module                Pointer 
+ * @return dl_module_t*  Pointer to next module
+ */
+dl_module_t *__dl_get_next_module(dl_module_t *module);
 
 #endif
