d4eb02e7152c92ddc7be11327f797daaf2b986c4
SpookyIluha <127010686+SpookyIluha@users.noreply.github.com>
2023-03-25 20:13:34 +0700

    Rework of the rdpq_set_tile, removed full version
    Added a struct to hold all the optional parameters the tile could hold for the rdpq_set_tile function

--- include/rdpq.h
+++ include/rdpq.h
@@ -253,6 +253,34 @@ typedef enum {
     TILE7 = 7,  ///< Tile #7 (for code readability)
 } rdpq_tile_t;
 
+
+/**
+ * @brief Tile parameters for #rdpq_set_tile_struct.
+ * 
+ * This structure contains all possible parameters for #rdpq_set_tile_struct.
+ * All fields have been made so that the 0 value is always the most
+ * reasonable default. This means that you can simply initialize the structure
+ * to 0 and then change only the fields you need (for instance, through a 
+ * compound literal).
+ * 
+ */
+typedef struct {
+	uint8_t  palette;	 ///< Optional palette associated to the texture. For textures in #FMT_CI4 format, specify the palette index (0-15), otherwise use 0.
+	
+	// Additional mapping parameters; Leave them as 0 if not required;
+	
+    bool     clamp_s; 	///< True if texture needs to be clamped in the S direction (U/X in UV/XY space). Otherwise wrap the texture around;
+	bool     mirror_s;	///< True if texture needs to be mirrored in the S direction (U/X in UV/XY space). Otherwise wrap the texture without mirroring;
+	uint8_t  mask_s;	///< Power of 2 boundary of the texture in pixels to wrap on in the S direction (V/Y in UV/XY space);
+	uint8_t  shift_s;	///< Power of 2 scale of the texture to wrap on in the S direction (V/Y in UV/XY space). Range is 0-15 dec;
+	
+    bool     clamp_t; 	///< True if texture needs to be clamped in the T direction (V/Y in UV/XY space). Otherwise wrap the texture around;
+	bool     mirror_t;	///< True if texture needs to be mirrored in the T direction (V/Y in UV/XY space). Otherwise wrap the texture without mirroring;
+	uint8_t  mask_t;	///< Power of 2 boundary of the texture in pixels to wrap on in the T direction (V/Y in UV/XY space);
+	uint8_t  shift_t;	///< Power of 2 scale of the texture to wrap on in the T direction (V/Y in UV/XY space). Range is 0-15 dec;
+	
+} rdpq_tileparms_t;
+
 /** @brief Tile descriptor internally used by some RDPQ functions. Avoid using if possible */
 #define RDPQ_TILE_INTERNAL           TILE7
 
@@ -724,52 +752,32 @@ inline void rdpq_load_block(rdpq_tile_t tile, uint16_t s0, uint16_t t0, uint16_t
     rdpq_load_block_fx(tile, s0, t0, num_texels, (2048 + words - 1) / words);
 }
 
-
-/**
- * @brief Enqueue a RDP SET_TILE command (full version)
- */
-inline void rdpq_set_tile_full(rdpq_tile_t tile, tex_format_t format, 
-    uint16_t tmem_addr, uint16_t tmem_pitch, uint8_t palette,
-    uint8_t cs, uint8_t ms, uint8_t mask_s, uint8_t shift_s,
-    uint8_t ct, uint8_t mt, uint8_t mask_t, uint8_t shift_t)
+/// @brief Enqueue a RDP SET_TILE command (struct version)
+/// @param[in] tile Tile descriptor index (0-7)
+/// @param[in] format Texture format for the tile. Cannot be 0. Should correspond to X_get_format in #surface_t or #sprite_t;
+/// @param[in] tmem_addr Address in tmem where the texture is (or will be loaded). Must be multiple of 8;
+/// @param[in] tmem_pitch Pitch of the texture in tmem in bytes. Must be multiple of 8. Should correspond to srtide in #surface_t;	
+/// @param[in] parms Additional optional parameters for the tile. Can be left NULL or all 0. More information about the struct is in rdpq_tileparms_t
+inline void rdpq_set_tile(rdpq_tile_t tile, 
+	tex_format_t format,
+    uint16_t tmem_addr,
+	uint16_t tmem_pitch,
+    const rdpq_tileparms_t *parms)
 {
+    static const rdpq_tileparms_t default_parms = {0};
+    if (!parms) parms = &default_parms;
+
     assertf((tmem_addr % 8) == 0, "invalid tmem_addr %d: must be multiple of 8", tmem_addr);
     assertf((tmem_pitch % 8) == 0, "invalid tmem_pitch %d: must be multiple of 8", tmem_pitch);
     extern void __rdpq_write8_syncchange(uint32_t, uint32_t, uint32_t, uint32_t);
     __rdpq_write8_syncchange(RDPQ_CMD_SET_TILE,
         _carg(format, 0x1F, 19) | _carg(tmem_pitch/8, 0x1FF, 9) | _carg(tmem_addr/8, 0x1FF, 0),
-        _carg(tile, 0x7, 24) | _carg(palette, 0xF, 20) | 
-        _carg(ct, 0x1, 19) | _carg(mt, 0x1, 18) | _carg(mask_t, 0xF, 14) | _carg(shift_t, 0xF, 10) | 
-        _carg(cs, 0x1, 9) | _carg(ms, 0x1, 8) | _carg(mask_s, 0xF, 4) | _carg(shift_s, 0xF, 0),
+        _carg(tile, 0x7, 24) | _carg(parms->palette, 0xF, 20) | 
+        _carg(parms->clamp_t, 0x1, 19) | _carg(parms->mirror_t, 0x1, 18) | _carg(parms->mask_t, 0xF, 14) | _carg(parms->shift_t, 0xF, 10) | 
+        _carg(parms->clamp_s, 0x1, 9) | _carg(parms->mirror_s, 0x1, 8) | _carg(parms->mask_s, 0xF, 4) | _carg(parms->shift_s, 0xF, 0),
         AUTOSYNC_TILE(tile));
 }
 
-/**
- * @brief Enqueue a RDP SET_TILE command (basic version)
- * 
- * This RDP command allows to configure one of the internal tile descriptors
- * of the RDP. A tile descriptor is used to describe property of a texture
- * either being loaded into TMEM, or drawn from TMEM into the target buffer.
- * 
- * @param[in]  tile        Tile descriptor index (0-7)
- * @param[in]  format      Texture format
- * @param[in]  tmem_addr   Address in tmem where the texture is (or will be loaded)
- * @param[in]  tmem_pitch  Pitch of the texture in tmem in bytes (must be multiple of 8)
- * @param[in]  palette     Optional palette associated to the texture. For textures in
- *                         #FMT_CI4 format, specify the palette index (0-15),
- *                         otherwise use 0.
- */
-inline void rdpq_set_tile(rdpq_tile_t tile, tex_format_t format, 
-    uint16_t tmem_addr, uint16_t tmem_pitch, uint8_t palette)
-{
-    assertf((tmem_addr % 8) == 0, "invalid tmem_addr %d: must be multiple of 8", tmem_addr);
-    assertf((tmem_pitch % 8) == 0, "invalid tmem_pitch %d: must be multiple of 8", tmem_pitch);
-    extern void __rdpq_write8_syncchange(uint32_t, uint32_t, uint32_t, uint32_t);
-    __rdpq_write8_syncchange(RDPQ_CMD_SET_TILE,
-        _carg(format, 0x1F, 19) | _carg(tmem_pitch/8, 0x1FF, 9) | _carg(tmem_addr/8, 0x1FF, 0),
-        _carg(tile, 0x7, 24) | _carg(palette, 0xF, 20),
-        AUTOSYNC_TILE(tile));
-}
 
 /**
  * @brief Enqueue a SET_FILL_COLOR RDP command.
