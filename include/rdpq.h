93087c8a72e95630cfe83cbc47f015612817eb3a
Giovanni Bajo <rasky@develer.com>
2023-05-25 11:32:02 +0200

    rdpq: move asserts from rdpq_set_tile_size to rdpq_set_tile_size_fx

--- include/rdpq.h
+++ include/rdpq.h
@@ -645,8 +645,6 @@ inline void rdpq_load_tlut_raw(rdpq_tile_t tile, uint8_t color_idx, uint8_t num_
  * @see #rdpq_set_tile_size_fx
  */
 #define rdpq_set_tile_size(tile, s0, t0, s1, t1) ({ \
-    assertf((s0) >= 0 && (t0) >= 0 && (s1) >= 0 && (t1) >= 0, "texture coordinates must be positive"); \
-    assertf((s0) <= 1024 && (t0) <= 1024 && (s1) <= 1024 && (t1) <= 1024, "texture coordinates must be smaller  1024"); \
     rdpq_set_tile_size_fx((tile), (s0)*4, (t0)*4, (s1)*4, (t1)*4); \
 })
 
@@ -667,6 +665,9 @@ inline void rdpq_load_tlut_raw(rdpq_tile_t tile, uint8_t color_idx, uint8_t num_
  */
 inline void rdpq_set_tile_size_fx(rdpq_tile_t tile, uint16_t s0, uint16_t t0, uint16_t s1, uint16_t t1)
 {
+    assertf((s0) >= 0 && (t0) >= 0 && (s1) >= 0 && (t1) >= 0, "texture coordinates must be positive");
+    assertf((s0) <= 1024*4 && (t0) <= 1024*4 && (s1) <= 1024*4 && (t1) <= 1024*4, "texture coordinates must be smaller than 1024");
+
     extern void __rdpq_write8_syncchange(uint32_t, uint32_t, uint32_t, uint32_t);
     __rdpq_write8_syncchange(RDPQ_CMD_SET_TILE_SIZE,
         _carg(s0, 0xFFF, 12) | _carg(t0, 0xFFF, 0),
