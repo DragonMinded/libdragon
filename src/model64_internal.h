def927cae2eb98404ecc0179161cf00b6ffcc165
Dennis Heinze <dennisjp.heinze@gmail.com>
2023-07-02 15:07:23 +0200

    model64: Fix some doxygen errors

--- src/model64_internal.h
+++ src/model64_internal.h
@@ -10,17 +10,17 @@
 /** @brief Current version of model64 */
 #define MODEL64_VERSION         1
 
-/** @brief Parameters for a single vertex attribute (part of #primitive_s) */
+/** @brief Parameters for a single vertex attribute (part of #primitive_t) */
 typedef struct attribute_s {
     uint32_t size;                  ///< Number of components per vertex. If 0, this attribute is not defined
-    uint32_t type;                  ///< The data type of each component (for example #GL_FLOAT)
+    uint32_t type;                  ///< The data type of each component (for example GL_FLOAT)
     uint32_t stride;                ///< The byte offset between consecutive vertices. If 0, the values are tightly packed
     void *pointer;                  ///< Pointer to the first value
 } attribute_t;
 
 /** @brief A single draw call that makes up part of a mesh (part of #mesh_t) */
 typedef struct primitive_s {
-    uint32_t mode;                  ///< Primitive assembly mode (for example #GL_TRIANGLES)
+    uint32_t mode;                  ///< Primitive assembly mode (for example GL_TRIANGLES)
     attribute_t position;           ///< Vertex position attribute, if defined
     attribute_t color;              ///< Vertex color attribyte, if defined
     attribute_t texcoord;           ///< Texture coordinate attribute, if defined
@@ -28,7 +28,7 @@ typedef struct primitive_s {
     attribute_t mtx_index;          ///< Matrix indices (aka bones), if defined
     uint32_t vertex_precision;      ///< If the vertex positions use fixed point values, this defines the precision
     uint32_t texcoord_precision;    ///< If the texture coordinates use fixed point values, this defines the precision
-    uint32_t index_type;            ///< Data type of indices (for example #GL_UNSIGNED_SHORT)
+    uint32_t index_type;            ///< Data type of indices (for example GL_UNSIGNED_SHORT)
     uint32_t num_vertices;          ///< Number of vertices
     uint32_t num_indices;           ///< Number of indices
     void *indices;                  ///< Pointer to the first index value. If NULL, indices are not used
@@ -42,7 +42,7 @@ typedef struct mesh_s {
 
 /** @brief A model64 file containing a model */
 typedef struct model64_s {
-    uint32_t magic;                 ///< Magic header (#MODEL64_MAGIC)
+    uint32_t magic;                 ///< Magic header (MODEL64_MAGIC)
     uint32_t version;               ///< Version of this file
     uint32_t header_size;           ///< Size of the header in bytes
     uint32_t mesh_size;             ///< Size of a mesh header in bytes
