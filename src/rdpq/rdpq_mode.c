e3a22d5c3ca1e9f206de471510769aefdbe801fd
nova <136651697+9nova@users.noreply.github.com>
2023-08-22 15:28:36 -0400

    add some missing `extern inline` definitions

--- src/rdpq/rdpq_mode.c
+++ src/rdpq/rdpq_mode.c
@@ -163,6 +163,7 @@ extern inline void rdpq_mode_zoverride(bool enable, float z, int16_t deltaz);
 extern inline void rdpq_mode_tlut(rdpq_tlut_t tlut);
 extern inline void rdpq_mode_filter(rdpq_filter_t s);
 extern inline void rdpq_mode_mipmap(rdpq_mipmap_t mode, int num_levels);
+extern inline void rdpq_mode_persp(bool perspective);
 ///@cond
 extern inline void __rdpq_mode_change_som(uint64_t mask, uint64_t val);
 ///@endcond
