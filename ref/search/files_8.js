c958489aeb594a492ef0e051c091f77e52c4dcbd
rasky <rasky@users.noreply.github.com>
2023-09-01 23:17:26 +0000

    deploy: 6a42ec7adf6f6a8e103436694841c3ba742c57a0

--- ref/search/files_8.js
+++ ref/search/files_8.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['joybus_2ec_0',['joybus.c',['../joybus_8c.html',1,'']]],
-  ['joybus_2eh_1',['joybus.h',['../joybus_8h.html',1,'']]]
+  ['joybus_2eh_1',['joybus.h',['../joybus_8h.html',1,'']]],
+  ['joybus_5finternal_2eh_2',['joybus_internal.h',['../joybus__internal_8h.html',1,'']]]
 ];
