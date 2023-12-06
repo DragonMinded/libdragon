6c0dea72da9054bd151fcad49f105e68bf01497d
Giovanni Bajo <rasky@develer.com>
2023-12-06 15:58:48 +0100

    mkasset: refactor asset_compress

--- tools/common/assetcomp.c
+++ tools/common/assetcomp.c
@@ -21,6 +21,68 @@
 
 #include "lz4_compress.h"
 
+void asset_compress_mem(int compression, const uint8_t *data, int sz, uint8_t **output, int *cmp_size, int *winsize, int *margin)
+{
+    switch (compression) {
+    case 1: { // lz4hc
+        // Default for LZ4HC is 8 KiB, which makes sense given the little
+        // data cache of VR4300 to improve decompression speed.
+        if (*winsize == 0) {
+            *winsize = 8*1024;
+            while (sz < *winsize && *winsize > 2*1024)
+                *winsize /= 2;
+        }
+
+        // The actual max distance of the LZ4 format is 64KiB-1, make sure we
+        // don't go over that.
+        if (*winsize > 64*1024) *winsize = 64*1024;
+        lz4_distance_max = *winsize;
+        if (lz4_distance_max > 65535) lz4_distance_max = 65535;
+
+        int cmp_max_size = LZ4_COMPRESSBOUND(sz);
+        *output = malloc(cmp_max_size);
+
+        // Compress the file. Use compression level LZ4HC_CLEVEL_MAX and
+        // "favor decompression speed", as we prefer to leave a bit of
+        // compression ratio on the table in exchange for faster decompression.
+        LZ4_streamHC_t* state = LZ4_createStreamHC();
+        LZ4_setCompressionLevel(state, LZ4HC_CLEVEL_MAX);
+        LZ4_favorDecompressionSpeed(state, 1);
+        *cmp_size = LZ4_compress_HC_continue(state, (char*)data, (char*)*output, sz, cmp_max_size);
+        LZ4_freeStreamHC(state);
+        assert(*cmp_size <= cmp_max_size);
+        *margin = LZ4_DECOMPRESS_INPLACE_MARGIN(*cmp_size);
+    }   break;
+    case 2: { // aplib
+        if (*winsize == 0) {
+            *winsize = 256*1024;
+            while (sz < *winsize && *winsize > 2*1024)
+                *winsize /= 2;
+        }
+    
+        apultra_stats stats;
+        int max_cmp_size = apultra_get_max_compressed_size(sz);
+        *output = calloc(1, max_cmp_size);  // note: apultra.c clears the buffer, not sure why
+        *cmp_size = apultra_compress(data, *output, sz, max_cmp_size, 
+            0,          // flags
+            *winsize,    // window size
+            0,          // dictionary size
+            NULL,       // progress callback
+            &stats);
+
+        *margin = stats.safe_dist + *cmp_size - sz;
+    }   break;
+    case 3: { // shrinkler
+        *winsize = 256*1024; // FIXME
+        int inplace_margin;
+        *output = shrinkler_compress(data, sz, 3, cmp_size, &inplace_margin);
+        *margin = inplace_margin;
+    }   break;
+    default:
+        assert(0);
+    }  
+}
+
 /**
  * @brief Compress or recompress a file in the libdragon asset format.
  * 
@@ -41,14 +103,11 @@ bool asset_compress(const char *infn, const char *outfn, int compression, int wi
 
     // Make sure the file exists before calling asset_load,
     // which would just assert.
-    int insize = 0;
     FILE *in = fopen(infn, "rb");
     if (!in) {
         fprintf(stderr, "error opening input file: %s\n", infn);
         return false;
     }
-    fseek(in, 0, SEEK_END);
-    insize = ftell(in);
     fclose(in);
 
     if (winsize && asset_winsize_to_flags(winsize) < 0) {
@@ -57,20 +116,21 @@ bool asset_compress(const char *infn, const char *outfn, int compression, int wi
         return false;
     }
 
-    // The caller specified a certain window size. We can still silently decrease it
-    // if the file is smaller, as there is no functional difference and we can save
-    // some RAM at decompression time.
-    if (winsize) {
-        while (insize < winsize && winsize > 2*1024)
-            winsize /= 2;
-    }
-
     // Load the file. This will transparently decompresses it if it was compressed,
     // so that this function can be used to also recompress an already compressed
     // file.
     int sz;
     uint8_t *data = asset_load(infn, &sz);
 
+    // The caller specified a certain window size. We can still silently decrease it
+    // if the file is smaller, as there is no functional difference and we can save
+    // some RAM at decompression time.
+    if (winsize) {
+        while (sz < winsize && winsize > 2*1024)
+            winsize /= 2;
+    }
+
+    // FIXME: use asset_compress_mem() instead of duplicating the code here
     switch (compression) {
     case 0: { // none
         FILE *out = fopen(outfn, "wb");
@@ -100,7 +160,7 @@ bool asset_compress(const char *infn, const char *outfn, int compression, int wi
     case 2: { // aplib
         if (winsize == 0) {
             winsize = 256*1024;
-            while (insize < winsize && winsize > 2*1024)
+            while (sz < winsize && winsize > 2*1024)
                 winsize /= 2;
         }
     
@@ -131,7 +191,7 @@ bool asset_compress(const char *infn, const char *outfn, int compression, int wi
         // data cache of VR4300 to improve decompression speed.
         if (winsize == 0) {
             winsize = 8*1024;
-            while (insize < winsize && winsize > 2*1024)
+            while (sz < winsize && winsize > 2*1024)
                 winsize /= 2;
         }
 
--- tools/common/assetcomp.h
+++ tools/common/assetcomp.h
@@ -8,5 +8,6 @@
 #define DEFAULT_WINSIZE_STREAMING    (4*1024)
 
 bool asset_compress(const char *infn, const char *outfn, int compression, int winsize);
+void asset_compress_mem(int compression, const uint8_t *inbuf, int size, uint8_t **outbuf, int *cmp_size, int *winsize, int *margin);
 
 #endif
