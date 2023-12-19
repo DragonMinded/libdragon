1312d4102426df3ee8562e3f5cd20a6daea180c8
Giovanni Bajo <rasky@develer.com>
2023-12-19 17:37:11 +0100

    tools: fix build under cygwin (though the platform is not officially supported)

--- tools/common/incbin.h
+++ tools/common/incbin.h
@@ -188,7 +188,7 @@
 #  if defined(INCBIN_ARM)
 /* On arm assemblers, `@' is used as a line comment token */
 #    define INCBIN_TYPE(NAME)    ".type " INCBIN_STRINGIZE(INCBIN_PREFIX) #NAME ", %object\n"
-#  elif defined(__MINGW32__) || defined(__MINGW64__)
+#  elif defined(__MINGW32__) || defined(__MINGW64__) || defined(__CYGWIN__)
 /* Mingw doesn't support this directive either */
 #    define INCBIN_TYPE(NAME)
 #  else
