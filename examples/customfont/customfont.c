cf06814b8dfbf6e1501790b3853cbc16e84f458b
Dennis Heinze <dennisjp.heinze@gmail.com>
2024-01-26 22:02:55 +0100

    Remove deprecated rdp functions from examples

--- examples/customfont/customfont.c
+++ examples/customfont/customfont.c
@@ -9,7 +9,6 @@ int main(void)
     /* Initialize peripherals */
     display_init( RESOLUTION_320x240, DEPTH_16_BPP, 2, GAMMA_NONE, FILTERS_RESAMPLE );
     dfs_init( DFS_DEFAULT_LOCATION );
-    rdp_init();
     controller_init();
     timer_init();
 
