DragonMinded/libdragon

commit 15c5cc09ea75a017c23a2d47100168687ed73921

mixer: fix a bug when using VADPCM and playing back faster than the sampling rate

src/audio/mixer.c
@@ -490,8 +490,15 @@ static void mixer_exec(int32_t *out, int num_samples) {
 			int len = ch->len >> bps_fx64;
 			int loop_len = ch->loop_len >> bps_fx64;
 			int wpos = ch->pos >> bps_fx64;
-			int wlast = (ch->pos + ch->step*(num_samples-1)) >> bps_fx64;
-			int wlen = wlast-wpos+1;
+			// Calculate how many samples we need to have available for this
+			// frame. We used to calculate the last sample as ch->step * (num_samples+1),
+			// but in the unlikely case the playing rate is much higher than the
+			// mixing rate, this might cause a seek in the waveform (eg: if we play
+			// one sample every 10, we don't want to cause a seek forward by 9,
+			// between the last sample of this frame and the first sample of
+			// next frame).
+			int wnext = (ch->pos + ch->step*num_samples) >> bps_fx64;
+			int wlen = MAX(wnext - wpos, 1);
 			assertf(wlen >= 0, "channel %d: wpos overflow", i);
 			tracef("ch:%d wpos:%x wlen:%x len:%x loop_len:%x sbuf_size:%x\n", i, wpos, wlen, len, loop_len, sbuf->size);
 

commit 74932d482d67050b5b9151cf1e1c0d025c794e8d

mixer: fix regression caused by 15c5cc0

src/audio/mixer.c
@@ -497,14 +497,17 @@ static void mixer_exec(int32_t *out, int num_samples) {
 			int loop_len = ch->loop_len >> bps_fx64;
 			int wpos = ch->pos >> bps_fx64;
 			// Calculate how many samples we need to have available for this
-			// frame. We used to calculate the last sample as ch->step * (num_samples+1),
-			// but in the unlikely case the playing rate is much higher than the
-			// mixing rate, this might cause a seek in the waveform (eg: if we play
+			// frame. We used to only calculate the last sample, but in the unlikely
+			// case the playback rate is much higher than the output rate,
+			// this might cause a seek in the waveform (eg: if we play
 			// one sample every 10, we don't want to cause a seek forward by 9,
 			// between the last sample of this frame and the first sample of
-			// next frame).
+			// next frame). Seeking creates problem with compressed streams, so
+			// we want to avoid it.
+			int wlast = (ch->pos + ch->step*(num_samples-1)) >> bps_fx64;
 			int wnext = (ch->pos + ch->step*num_samples) >> bps_fx64;
-			int wlen = MAX(wnext - wpos, 1);
+			int wlen = MAX(wlast-wpos+1, wnext-wpos);
+
 			assertf(wlen >= 0, "channel %d: wpos overflow", i);
 			tracef("ch:%d wpos:%x wlen:%x len:%x loop_len:%x sbuf_size:%x\n", i, wpos, wlen, len, loop_len, sbuf->size);
 

src/audio/wav64.c
@@ -190,6 +190,11 @@ static void waveform_vadpcm_read(void *ctx, samplebuffer_t *sbuf, int wpos, int 
 	bool highpri = false;
 	while (wlen > 0) {
 		int nframes = wlen / 16;
+		// Most of the code here would be ready to loop over multiple blocks of
+		// 256 frames, but the problem is that we don't doublebuffer the RDRAM
+		// buffers, so the RSP doesn't get to process the data in time. This
+		// would require CPU-spinning here. Since it's a very rare case, just
+		// block it for now.
 		assert(nframes <= 256);
 		nframes = MIN(nframes, 256);
 