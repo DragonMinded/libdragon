ffe0ca19129d227b2e62c957e751d5898dffe526
Giovanni Bajo <rasky@develer.com>
2024-04-22 23:56:41 +0200

    Vendor https://github.com/emmanuel-marty/apultra

---
+++ tools/common/aplib_compress.c
@@ -0,0 +1,9 @@
+// apultra
+#include "apultra/matchfinder.c"
+#include "apultra/shrink.c"
+
+// libdivsufsort
+#include "apultra/divsufsort.c"
+#include "apultra/divsufsort_utils.c"
+#include "apultra/sssort.c"
+#include "apultra/trsort.c"
