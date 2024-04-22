2713dba9cca0301b0abac4546ac67503df9b821a
rasky <rasky@users.noreply.github.com>
2024-04-22 16:22:46 +0000

    deploy: 1e6c006495743f1cce5d7a1ec56514c87df8c0ca

--- ref/search/defines_5.js
+++ ref/search/defines_5.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['function_5falignment_0',['FUNCTION_ALIGNMENT',['../backtrace_8c.html#afcfa885f92ac4859c340d39a3ac8cd48',1,'backtrace.c']]]
+  ['fps_5fwindow_0',['FPS_WINDOW',['../display_8c.html#aaa6d18e9341ec1912379aeb12e618f02',1,'display.c']]],
+  ['function_5falignment_1',['FUNCTION_ALIGNMENT',['../backtrace_8c.html#afcfa885f92ac4859c340d39a3ac8cd48',1,'backtrace.c']]]
 ];
