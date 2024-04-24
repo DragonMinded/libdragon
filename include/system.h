e5a385ee7a0f99ed9ef1bde910f46710af0f69a6
Giovanni Bajo <rasky@develer.com>
2024-04-25 00:37:13 +0200

    system: improve syscall layer by using O(1) fileno lookups
    Currently, the syscall layer uses a 100-element array of 12-byte
    file descriptors, that are often searched linearly when looking them
    up. This is not very efficient, wastes a bit of memory, and is acceptable
    only because normally games don't have many open files.
    
    Since we want to support more complex file-based applications like
    file managers, we improve a bit this architecture. We switch to use
    integer file numbers that encode the filesystem index and descriptor
    index in their bits, so that looking up a fileno is O(1). File descritors
    become just 4 bytes (which is just the underlying file descriptor created
    by the filesystem implementation), and we store them in a trie-like
    structure that starts by 32 entries and grows only when needed. Now up
    to 4096 open files are supported, but the memory is allocated only
    if needed.

--- include/system.h
+++ include/system.h
@@ -13,8 +13,6 @@
 
 /** @brief Number of filesystems that can be attached to the system */
 #define MAX_FILESYSTEMS     10
-/** @brief Number of open handles that can be maintained at one time */
-#define MAX_OPEN_HANDLES    100
 
 #ifdef __cplusplus
 extern "C" {
