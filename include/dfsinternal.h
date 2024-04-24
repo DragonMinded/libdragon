82377d347abb946e0fb4e61ffe6c3c3db933dc6a
Giovanni Bajo <rasky@develer.com>
2024-04-25 00:21:20 +0200

    dragonfs: remove limit of 4 open files
    Currently, DragonFS uses a fixed global array of 4 pre-allocated file
    descriptors. This commit lifts the limit by switching to dynamic
    allocation of file descriptors via malloc. Given that a descriptor now
    is very small (12 bytes) since we removed the buffer cache in the
    previous commit, allocation fits small object pools and is very fast.
    
    Given that the DFS API was designed to use positive numbers for
    file handles, and we don't have indices anymore, we switch to use the
    physical address of the descriptor as file handle.

--- include/dfsinternal.h
+++ include/dfsinternal.h
@@ -42,17 +42,15 @@ _Static_assert(sizeof(struct directory_entry) == SECTOR_SIZE, "invalid directory
 typedef struct directory_entry directory_entry_t;
 
 /** @brief Open file handle structure */
-typedef struct open_file
+typedef struct dfs_open_file_s
 {
-    /** @brief The unique file handle to refer to this file by */
-    uint32_t handle;
     /** @brief The size in bytes of this file */
     uint32_t size;
     /** @brief The offset of the current location in the file */
     uint32_t loc;
     /** @brief The offset within the filesystem where the file is stored */
     uint32_t cart_start_loc;
-} open_file_t;
+} dfs_open_file_t;
 
 /** @} */ /* dfs */
 
