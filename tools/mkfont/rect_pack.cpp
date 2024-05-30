be09ef31413473152473beb94e925a74cd834da4
Giovanni Bajo <rasky@develer.com>
2024-05-31 00:25:50 +0200

    mkfont: try avoiding GCC errors

--- tools/mkfont/rect_pack.cpp
+++ tools/mkfont/rect_pack.cpp
@@ -1,4 +1,17 @@
+#ifdef __clang__
+    #pragma clang diagnostic push
+#else
+    #pragma GCC diagnostic push
+    #pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
+#endif
+
 #include "rect_pack/rect_pack.h"
 #include "rect_pack/rect_pack.cpp"
 #include "rect_pack/stb_rect_pack.cpp"
 #include "rect_pack/MaxRectsBinPack.cpp"
+
+#ifdef __clang__
+    #pragma clang diagnostic pop
+#else
+    #pragma GCC diagnostic pop
+#endif
