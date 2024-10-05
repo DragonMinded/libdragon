1371d094351dcf1626bec9ac88378f25f01a13a9
Giovanni Bajo <rasky@develer.com>
2024-10-05 22:43:06 +0200

    mkfontbuiltin: avoid xxd -n which is not everywhere yet

--- src/rdpq/mkfontbuiltin.sh
+++ src/rdpq/mkfontbuiltin.sh
@@ -10,5 +10,7 @@ BUILTIN="monogram at01"
 rm -f rdpq_font_builtin.c
 for font in $BUILTIN; do
     "$N64_INST/bin/mkfont" --compress 0 --output $TMPDIR --range 20-BF --outline 1  "$@" "$FONTDB/$font.ttf"
-    xxd -i -n __fontdb_$font $TMPDIR/$font.font64 >>rdpq_font_builtin.c
+    OUTFN="$TMPDIR/$font.font64"
+    OUTFN_C=$(echo $OUTFN | sed 's/[/.]/_/g')
+    xxd -i $OUTFN | sed "s/${OUTFN_C}/__fontdb_${font}/g" >>rdpq_font_builtin.c
 done
