2c0708447db87a15f4cc7881abd5ad1f2a599cb9
Giovanni Bajo <rasky@develer.com>
2024-10-11 21:41:18 +0200

    tools: add C++ header guards to assetcomp.h

--- tools/common/assetcomp.h
+++ tools/common/assetcomp.h
@@ -7,7 +7,15 @@
 // Default window size for streaming decompression (asset_fopen())
 #define DEFAULT_WINSIZE_STREAMING    (4*1024)
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 bool asset_compress(const char *infn, const char *outfn, int compression, int winsize);
 void asset_compress_mem(int compression, const uint8_t *inbuf, int size, uint8_t **outbuf, int *cmp_size, int *winsize, int *margin);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
