e5c05313d40e41c5678e2dc3e064acff52bedea9
Giovanni Bajo <rasky@develer.com>
2024-10-13 21:49:00 +0200

    rdpq: fix rdpq_tex_upload_tlut to support any index range
    (cherry picked from commit d60b6df7a756adf56ac97c700b123383b4e20f30)

--- tests/testrom.c
+++ tests/testrom.c
@@ -318,6 +318,7 @@ static const struct Testsuite
 	TEST_FUNC(test_rdpq_tex_upload_multi,      0, TEST_FLAGS_NO_BENCHMARK),
 	TEST_FUNC(test_rdpq_tex_blit_normal,       0, TEST_FLAGS_NO_BENCHMARK),
 	TEST_FUNC(test_rdpq_tex_multi_i4,          0, TEST_FLAGS_NO_BENCHMARK),
+	TEST_FUNC(test_rdpq_tex_upload_tlut,       0, TEST_FLAGS_NO_BENCHMARK),
 	TEST_FUNC(test_rdpq_sprite_upload,         0, TEST_FLAGS_NO_BENCHMARK),
 	TEST_FUNC(test_rdpq_sprite_lod,            0, TEST_FLAGS_NO_BENCHMARK),
 };
