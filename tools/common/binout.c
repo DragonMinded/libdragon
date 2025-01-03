4d5e46f385b7d3f093c991ec0bf5a03335a65de5
gamemasterplc <gamemasterplc@gmail.com>
2025-01-03 17:15:57 -0600

    tools: improve binout library to add support to placeholders
    binout is used by tools to generate binary files. This change adds
    support for "placeholders" which is a way to leave an empty space
    in a binary file to be filled up later. A very common case is storing
    offsets to further sections in the file.
    
    Moreover, the basic wNN() functions are improved to assert in case
    the variable being passed does not fit the specified bitsize, to avoid
    silent truncations.

--- tools/common/binout.c
+++ tools/common/binout.c
@@ -1,22 +1,44 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdarg.h>
 #include <assert.h>
+#include "binout.h"
 
-void w8(FILE *f, uint8_t v) 
+#define STBDS_NO_SHORT_NAMES
+#define STB_DS_IMPLEMENTATION //Hack to get tools to compile
+#include "stb_ds.h"
+
+struct placeholder_data {
+	int offset;
+	int *pending_offsets;
+};
+
+struct {
+	char *key;
+	struct placeholder_data value;
+} *placeholder_hash = NULL;
+
+void _w8(FILE *f, uint8_t v) 
 {
     fputc(v, f);
 }
 
-void w16(FILE *f, uint16_t v)
+void _w16(FILE *f, uint16_t v)
 {
     w8(f, v >> 8);
     w8(f, v & 0xff);
 }
 
-void w32(FILE *f, uint32_t v)
+void _w32(FILE *f, uint32_t v)
 {
     w16(f, v >> 16);
     w16(f, v & 0xffff);
+}
+
+void _w64(FILE *f, uint64_t v)
+{
+    w32(f, v >> 32);
+    w32(f, v & 0xffffffff);
 }
 
 int w32_placeholder(FILE *f)
@@ -47,4 +69,99 @@ void wpad(FILE *f, int size)
     while (size--) {
         w8(f, 0);
     }
+}
+
+struct placeholder_data *__placeholder_get_data(const char *name)
+{
+	ptrdiff_t index = stbds_shgeti(placeholder_hash, name);
+	if(index == -1) {
+		struct placeholder_data default_value = {-1, NULL};
+		index = stbds_shlen(placeholder_hash);
+		stbds_shput(placeholder_hash, name, default_value);
+	}
+	return &placeholder_hash[index].value;
+}
+
+void __placeholder_make(FILE *file, int offset, const char *name)
+{
+	if(placeholder_hash == NULL) {
+		stbds_sh_new_arena(placeholder_hash);
+	}
+	struct placeholder_data *data = __placeholder_get_data(name);
+	data->offset = offset;
+	for(size_t i=0; i<stbds_arrlenu(data->pending_offsets); i++) {
+		w32_at(file, data->pending_offsets[i], data->offset);
+	}
+	stbds_arrfree(data->pending_offsets);
+}
+
+void placeholder_setv(FILE *file, const char *format, va_list arg)
+{
+	char *name = NULL;
+	vasprintf(&name, format, arg);
+	__placeholder_make(file, ftell(file), name);
+	free(name);
+}
+
+void placeholder_set(FILE *file, const char *format, ...)
+{
+	va_list args;
+	va_start(args, format);
+	placeholder_setv(file, format, args);
+	va_end(args);
+}
+
+void placeholder_setv_offset(FILE *file, int offset, const char *format, va_list arg)
+{
+	char *name = NULL;
+	vasprintf(&name, format, arg);
+	__placeholder_make(file, offset, name);
+	free(name);
+}
+
+void placeholder_set_offset(FILE *file, int offset, const char *format, ...)
+{
+	va_list args;
+	va_start(args, format);
+	placeholder_setv_offset(file, offset, format, args);
+	va_end(args);
+}
+
+
+void __w32_placeholder_named(FILE *file, const char *name)
+{
+	if(placeholder_hash == NULL) {
+		stbds_sh_new_arena(placeholder_hash);
+	}
+	struct placeholder_data *data = __placeholder_get_data(name);
+	if(data->offset == -1) {
+		stbds_arrpush(data->pending_offsets, ftell(file));
+		w32(file, 0);
+	} else {
+		w32(file, data->offset);
+	}
+}
+
+void w32_placeholdervf(FILE *file, const char *format, va_list arg)
+{
+	char *name = NULL;
+	vasprintf(&name, format, arg);
+	__w32_placeholder_named(file, name);
+	free(name);
+}
+
+void w32_placeholderf(FILE *file, const char *format, ...)
+{
+	va_list args;
+	va_start(args, format);
+	w32_placeholdervf(file, format, args);
+	va_end(args);
+}
+
+void placeholder_clear()
+{
+	for(size_t i=0; i<stbds_shlenu(placeholder_hash); i++) {
+		stbds_arrfree(placeholder_hash[i].value.pending_offsets);
+	}
+	stbds_shfree(placeholder_hash);
 }
--- tools/common/binout.h
+++ tools/common/binout.h
@@ -9,17 +9,53 @@
 #include <stdio.h>
 #include <assert.h>
 #include <stdint.h>
+#include <stdarg.h>
+#include <math.h>
 
-#define BITCAST_F2I(f)   ({ uint32_t __i; memcpy(&__i, &(f), 4); __i; })
+#define BITCAST_F2I(f)   ({ uint32_t __i; float __f = (f); memcpy(&__i, &(__f), 4); __i; })
 
-void w8(FILE *f, uint8_t v);
-void w16(FILE *f, uint16_t v);
-void w32(FILE *f, uint32_t v);
-#define wf32(f, v) w32(f, BITCAST_F2I(v))
+#define _wconv(type, v) ({ \
+    typeof(v) _v = (v); \
+    if (sizeof(type) < sizeof(_v)) { \
+        int64_t ext = (int64_t)_v >> (sizeof(type) * 8); \
+        if (ext != 0 && ext != (uint64_t)-1) { \
+            fprintf(stderr, "fatal: truncating value %lld to %s (ext=%lld)\n", (long long)_v, #type, (long long)ext); \
+            assert(ext == 0 || ext == (uint64_t)-1); \
+        } \
+    } \
+    (type)_v; \
+})
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void placeholder_setv(FILE *file, const char *format, va_list arg);
+void placeholder_set(FILE *file, const char *format, ...);
+void placeholder_setv_offset(FILE *file, int offset, const char *format, va_list arg);
+void placeholder_set_offset(FILE *file, int offset, const char *format, ...);
+void placeholder_clear();
+
+void _w8(FILE *f, uint8_t v);
+void _w16(FILE *f, uint16_t v);
+void _w32(FILE *f, uint32_t v);
+void _w64(FILE *f, uint64_t v);
+#define w8(f, v) _w8(f, _wconv(uint8_t, v))
+#define w16(f, v) _w16(f, _wconv(uint16_t, v))
+#define w32(f, v) _w32(f, _wconv(uint32_t, v))
+#define w64(f, v) _w64(f, _wconv(uint64_t, v))
+#define wf32(f, v) _w32(f, BITCAST_F2I(v))
+#define wf32approx(f, v, prec) wf32(f, roundf((v)/(prec))*(prec))
 
 int w32_placeholder(FILE *f);
+void w32_placeholdervf(FILE *file, const char *format, va_list arg);
+void w32_placeholderf(FILE *file, const char *format, ...);
 void w32_at(FILE *f, int pos, uint32_t v);
 void walign(FILE *f, int align);
 void wpad(FILE *f, int size);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
