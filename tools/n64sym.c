42c08d109e41c7fa51332dc09bf706d25e078462
Giovanni Bajo <rasky@develer.com>
2025-01-06 22:39:14 +0100

    n64sym: switch to standard binout library

--- tools/n64sym.c
+++ tools/n64sym.c
@@ -5,13 +5,12 @@
 #include <stdbool.h>
 #include <stdarg.h>
 
-#define STBDS_NO_SHORT_NAMES
-#define STB_DS_IMPLEMENTATION
-#include "common/stb_ds.h"
-
 #include "common/subprocess.h"
 #include "common/polyfill.h"
 #include "common/utils.h"
+#include "common/binout.h"
+
+#include "common/binout.c"
 
 bool flag_verbose = false;
 int flag_max_sym_len = 64;
@@ -71,30 +70,6 @@ int stringtable_add(char *word)
         word[i] = ch;
     }
     return idx;
-}
-
-#define conv(type, v) ({ \
-    typeof(v) _v = (v); assert((type)_v == _v); (type)_v; \
-})
-
-void _w8(FILE *f, uint8_t v)  { fputc(v, f); }
-void _w16(FILE *f, uint16_t v) { _w8(f, v >> 8); _w8(f, v & 0xff); }
-void _w32(FILE *f, uint32_t v) { _w16(f, v >> 16); _w16(f, v & 0xffff); }
-#define w8(f, v) _w8(f, conv(uint8_t, v))
-#define w16(f, v) _w16(f, conv(uint16_t, v))
-#define w32(f, v) _w32(f, conv(uint32_t, v))
-
-int w32_placeholder(FILE *f) { int pos = ftell(f); w32(f, 0); return pos; }
-void w32_at(FILE *f, int pos, uint32_t v)
-{
-    int cur = ftell(f);
-    fseek(f, pos, SEEK_SET);
-    w32(f, v);
-    fseek(f, cur, SEEK_SET);
-}
-void walign(FILE *f, int align) { 
-    int pos = ftell(f);
-    while (pos++ % align) w8(f, 0);
 }
 
 struct symtable_s {
