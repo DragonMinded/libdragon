b0b440ddb7d9299b38cea584af7399f2e51773a5
Giovanni Bajo <rasky@develer.com>
2025-01-06 23:20:19 +0100

    rsp.h: fix a pasto in docs

--- include/rsp.h
+++ include/rsp.h
@@ -232,7 +232,7 @@ extern "C" {
 #define SP_WSTATUS_CLEAR_SSTEP       0x00020   ///< SP_STATUS write mask: clear SSTEP bit
 #define SP_WSTATUS_SET_SSTEP         0x00040   ///< SP_STATUS write mask: set SSTEP bit
 #define SP_WSTATUS_CLEAR_INTR_BREAK  0x00080   ///< SP_STATUS write mask: clear #SP_STATUS_INTERRUPT_ON_BREAK bit
-#define SP_WSTATUS_SET_INTR_BREAK    0x00100   ///< SP_STATUS write mask: set SSTEP bit
+#define SP_WSTATUS_SET_INTR_BREAK    0x00100   ///< SP_STATUS write mask: set #SP_STATUS_INTERRUPT_ON_BREAK bit
 #define SP_WSTATUS_CLEAR_SIG0        0x00200   ///< SP_STATUS write mask: clear SIG0 bit
 #define SP_WSTATUS_SET_SIG0          0x00400   ///< SP_STATUS write mask: set SIG0 bit
 #define SP_WSTATUS_CLEAR_SIG1        0x00800   ///< SP_STATUS write mask: clear SIG1 bit
