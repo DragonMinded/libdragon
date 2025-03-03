6e2c533db17e212e014ecdc9ecc191bcd3020b3c
Giovanni Bajo <rasky@develer.com>
2025-03-03 13:12:58 +0100

    ed64x: correctly update also joybus RTC cache

--- src/ed64x.c
+++ src/ed64x.c
@@ -9,11 +9,13 @@
 #include "ed64x.h"
 #include "rtc_internal.h"
 #include "utils.h"
+#include "n64types.h"
 
 #define DS1337_DEVICE_ADDR      0x68            ///< DS1337 I2C device address
 
 static const uint32_t ED64X_REG_I2C_CMD = 0x1F800018;
 static const uint32_t ED64X_REG_I2C_DAT = 0x1F80001C;
+static const uint32_t ED64X_REG_RTC_CACHE = 0x1F808010;
 
 static int ed64x_i2c_status(void)
 {
@@ -90,5 +92,33 @@ static int ed64x_i2c_write(uint16_t addr, const uint8_t* data, int len)
 
 int ed64x_rtc_write(uint8_t buf[7])
 {
-    return ed64x_i2c_write((DS1337_DEVICE_ADDR << 9) + 0, buf, 7);
+    // Update the actual RTC unit via I2C
+    int ret = ed64x_i2c_write((DS1337_DEVICE_ADDR << 9) + 0, buf, 7);
+    if (ret < 0)
+        return ret;
+
+    // Now that the RTC has been changed, update the cached value exposed via the
+    // joybus interface
+    uint8_t buffer[8] = {0};
+
+    // Stop the clock
+    io_write(ED64X_REG_RTC_CACHE + 0, *(u_uint32_t*)(buffer + 0));
+    io_write(ED64X_REG_RTC_CACHE + 4, *(u_uint32_t*)(buffer + 4));
+
+    // Setup the buffer in *joybus* format. This is quite similar to DS1337 format,
+    // but there is a swap between the day of week and the day of month.
+    memcpy(buffer, buf, 7);
+    buffer[0] = buf[0];
+    buffer[1] = buf[1];
+    buffer[2] = buf[2];
+    buffer[3] = buf[4];     
+    buffer[4] = buf[3] - 1; // day of week is 1-indexed in DS1337, and 0-indexed on joybus
+    buffer[5] = buf[5];
+    buffer[6] = buf[6];
+    buffer[7] = 0x01; // special start-stop register value (?)
+
+    io_write(ED64X_REG_RTC_CACHE + 0, *(u_uint32_t*)(buffer + 0));
+    io_write(ED64X_REG_RTC_CACHE + 4, *(u_uint32_t*)(buffer + 4));
+
+    return 0;
 }
