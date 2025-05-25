92cea515c2809fb047a7c545ffcf56f0cdd995a5
SpookyIluha <127010686+SpookyIluha@users.noreply.github.com>
2025-05-26 04:00:35 +0700

    Update examples to disable filters where appropriate (#713)
    Update examples to disable filters where appropriate

--- examples/rtctest/rtctest.c
+++ examples/rtctest/rtctest.c
@@ -22,7 +22,7 @@ int main(void)
     debug_init_isviewer();
     debug_init_usblog();
 
-    display_init( RESOLUTION_320x240, DEPTH_32_BPP, 2, GAMMA_NONE, FILTERS_RESAMPLE );
+    display_init( RESOLUTION_320x240, DEPTH_32_BPP, 2, GAMMA_NONE, FILTERS_DISABLED );
     joypad_init();
     timer_init();
 
