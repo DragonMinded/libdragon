45224b6fe925a2b55aab8735dc81e6958c63e0f7
Giovanni Bajo <rasky@develer.com>
2025-07-19 01:07:45 +0200

    loadspritefromsd: modernize with sprite and joypad API

--- examples/loadspritefromsd/loadspritefromsd.c
+++ examples/loadspritefromsd/loadspritefromsd.c
@@ -4,72 +4,44 @@
 #include <stdint.h>
 #include <libdragon.h>
 
-static const int MAX_SPRITES = 3;
+static const int MAX_SPRITES = 4;
 static char *sprites_rom[] = {
-	"rom:/mudkip.sprite",
-	"rom:/earthbound.sprite",
-	"rom:/plane.sprite",
+	"rom:/attack1.sprite",
+	"rom:/attack2.sprite",
+	"rom:/attack3.sprite",
+	"rom:/attack4.sprite",
 };
+
 static char *sprites_sd[] = {
-	"sd:/mudkip.sprite",
-	"sd:/earthbound.sprite",
-	"sd:/plane.sprite",
+	"sd:/attack1.sprite",
+	"sd:/attack2.sprite",
+	"sd:/attack3.sprite",
+	"sd:/attack4.sprite",
 };
 static bool use_sd = true;
 
 static int cur_sprite = -1;
 static sprite_t *sprite;
 
-/* Gets the file size - used to know how much to allocate */
-int filesize(FILE *pFile) {
-	fseek(pFile, 0, SEEK_END);
-	int lSize = ftell(pFile);
-	rewind(pFile);
-
-	return lSize;
-}
-
-/*
-Load a sprite with the given path. Can load both from SD and ROM.
-We can't use dfs_read here since we may be loading from the SD card.
- */
-sprite_t *spritesheet_load(const char *sprite_path) {
-	FILE *fp = fopen(sprite_path, "r");
-	if (!fp) {
-		debugf("Error loading background\n");
-		return NULL;
-	}
-
-	const int size = filesize(fp);
-	debugf("Size: %d\n", size);
-
-	sprite_t *sprite = malloc(size);
-
-	fread(sprite, 1, size, fp);
-	fclose(fp);
-
-	return sprite;
-}
-
 /* Selects the next sprite and loads it (also frees the previous, if any) */
 void load_sprite(int id) {
 	if (cur_sprite >= 0) {
-		free(sprite);
+		sprite_free(sprite);
 	}
 
 	cur_sprite = id;
 
 	if (use_sd)
-		sprite = spritesheet_load(sprites_sd[cur_sprite]);
+		sprite = sprite_load(sprites_sd[cur_sprite]);
 	else
-		sprite = spritesheet_load(sprites_rom[cur_sprite]);
+		sprite = sprite_load(sprites_rom[cur_sprite]);
 }
 
 int main(void) {
 	/* Initialize peripherals */
 	display_init(RESOLUTION_320x240, DEPTH_16_BPP, 2, GAMMA_NONE, FILTERS_RESAMPLE);
 	dfs_init(DFS_DEFAULT_LOCATION);
-	controller_init();
+	joypad_init();
 
 	/* This will initialize the SD filesystem using 'sd:/' to identify it */
 	/* This path has to be the same used by the sprites when loading */
@@ -89,7 +61,7 @@ int main(void) {
 		graphics_fill_screen(disp, 0);
 
 		/* Draw the current loaded sprite (can be from SD or ROM, at this point it doesn't matter) */
-		graphics_draw_sprite(disp, 20, 40, sprite);
+		graphics_draw_sprite_trans(disp, 20, 40, sprite);
 
 		/* Draw some help text on screen */
 		graphics_draw_text(disp, 20, 20, "Press START to change sprites.");
@@ -102,10 +74,10 @@ int main(void) {
 		display_show(disp);
 
 		/* Do we need to change the sprite? */
-		controller_scan();
-		struct controller_data keys = get_keys_down();
+		joypad_poll();
+		joypad_buttons_t keys = joypad_get_buttons_pressed(JOYPAD_PORT_1);
 
-		if (keys.c[0].start) {
+		if (keys.start) {
 			/* Load the next sprite */
 			const int id = (cur_sprite + 1) % MAX_SPRITES;
 			load_sprite(id);
