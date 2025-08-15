209bcf73a6a65b2ca321f10507cd21e010ab184a
Giovanni Bajo <rasky@develer.com>
2025-08-15 09:37:54 +0200

    cpaktool: add header and license

--- tools/cpaktool/cpaktool.h
+++ tools/cpaktool/cpaktool.h
@@ -1,3 +1,13 @@
+/*
+    cpaktool - Controller Pak manipulation tool
+    Written by Giovanni Bajo <giovannibajo@gmail.com>
+
+    This tool is part of the Libdragon SDK.
+
+    This is free and unencumbered software released into the public domain.
+
+    For more information, please refer to <http://unlicense.org/>
+*/
 #ifndef CPAKTOOL_H
 #define CPAKTOOL_H
 
