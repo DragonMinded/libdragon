805a24cfb1dc6b20b67b6a0d96029722ec5e5482
Giovanni Bajo <rasky@develer.com>
2025-08-31 02:11:57 +0200

    Vendor plutosvg (789067a)

---
+++ tools/mkfont/plutosvg/plutosvg-amalgam.c
@@ -0,0 +1,15 @@
+#include "plutosvg.c"
+#undef MAX_NAME
+#include "plutovg-blend.c"
+#include "plutovg-canvas.c"
+#include "plutovg-font.c"
+#include "plutovg-matrix.c"
+#include "plutovg-paint.c"
+#include "plutovg-path.c"
+#include "plutovg-rasterize.c"
+#include "plutovg-surface.c"
+#include "plutovg-ft-math.c"
+#undef PVG_FT_END_STMNT
+#include "plutovg-ft-raster.c"
+#include "plutovg-ft-stroker.c"
+
