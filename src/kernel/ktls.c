07a2ac30ee4fd2e19330d82d6c3a9c8d3f5a37fb
Giovanni Bajo <rasky@develer.com>
2025-09-18 23:20:39 +0200

    kernel: fix bug accessing TLS in main thread

--- src/kernel/ktls.c
+++ src/kernel/ktls.c
@@ -6,7 +6,4 @@
 #include "ktls_internal.h"
 
 /** Initialize TLS pointer to the tls_base area in RAM */
-void *th_cur_tp = __tls_base + TP_OFFSET;
-
-/** @brief Current thread TLS pointer */
-void *__th_cur_tp;
+void *__th_cur_tp = __tls_base + TP_OFFSET;
