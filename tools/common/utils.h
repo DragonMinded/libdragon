7094305ad63035e73cf5db0e0722aa300561901c
Giovanni Bajo <rasky@develer.com>
2025-09-22 22:28:50 +0200

    tools: rework makefile (and some tool) to work with custom GCC triplets

--- tools/common/utils.h
+++ tools/common/utils.h
@@ -11,11 +11,46 @@
 #ifndef LIBDRAGON_TOOLS_UTILS_H
 #define LIBDRAGON_TOOLS_UTILS_H
 
+#include "polyfill.h"
 #include "../../src/utils.h"
 
+#include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
+#include <sys/stat.h>
 
+__attribute__((used))
+static char* path_remove_trailing_slash(char *path)
+{
+    path = strdup(path);
+    int n = strlen(path);
+    if (path[n-1] == '/' || path[n-1] == '\\')
+        path[n-1] = 0;
+    return path;
+}
+
+__attribute__((used))
+static char *change_ext(const char *fn, const char *ext)
+{
+    char *out = strdup(fn);
+    char *dot = strrchr(out, '.');
+    if (dot) *dot = 0;
+    strcat(out, ext);
+    return out;
+}
+
+__attribute__((used))
+static bool file_exists(const char *filename)
+{
+    FILE *f = fopen(filename, "r");
+    if (f) fclose(f);
+    return f != NULL;
+}
+
+// Find the directory where the libdragon toolchain is installed.
+// This is where you can find GCC, the linker, etc.
+__attribute__((used))
 static const char *n64_toolchain_dir(void)
 {
     static char *n64_inst = NULL;
@@ -36,12 +71,57 @@ static const char *n64_toolchain_dir(void)
     // Remove the trailing backslash if any. On some system, running
     // popen with a path containing double backslashes will fail, so
     // we normalize it here.
-    n64_inst = strdup(n64_inst);
-    int n = strlen(n64_inst);
-    if (n64_inst[n-1] == '/' || n64_inst[n-1] == '\\')
-        n64_inst[n-1] = 0;
-
+    n64_inst = path_remove_trailing_slash(n64_inst);
     return n64_inst;
 }
 
+// Find the prefix to be prepended for GCC commands.
+// This is everything before gcc, ld, objdump, etc
+__attribute__((used))
+static const char *n64_gccprefix_triplet(void)
+{
+    static char *n64_gccprefix_triplet = NULL;
+    if (n64_gccprefix_triplet)
+        return n64_gccprefix_triplet;
+
+    const char *inst = n64_toolchain_dir();
+    if (!inst)
+        return NULL;
+    const char *target = getenv("N64_TARGET");
+    if (!target)
+        target = "mips64-elf";
+
+    int ret;
+    if (target[0])
+        ret = asprintf(&n64_gccprefix_triplet, "%s/bin/%s-", inst, target);
+    else
+        ret = asprintf(&n64_gccprefix_triplet, "%s/bin/", inst);
+
+    if (ret < 0) {
+        perror("asprintf");
+        exit(1);
+    }
+    return n64_gccprefix_triplet;
+}
+
+// Find the directory where the libdragon tools are installed.
+// This is where you can find mksprite, mkfont, etc.
+__attribute__((used))
+static const char *n64_tools_dir(void)
+{
+    static char *n64_inst = NULL;
+    if (n64_inst)
+        return n64_inst;
+
+    // Find the tools installation directory.
+    n64_inst = getenv("N64_INST");
+    if (!n64_inst)
+        return NULL;
+
+    // Remove the trailing backslash if any. On some system, running
+    // popen with a path containing double backslashes will fail, so
+    // we normalize it here.
+    n64_inst = path_remove_trailing_slash(n64_inst);
+    return n64_inst;
+}
 #endif
