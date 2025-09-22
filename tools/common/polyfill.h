0cd8700b2790e8a07375d206d1afdd69fa184665
Giovanni Bajo <rasky@develer.com>
2025-09-22 22:34:43 +0200

    tools: improve mingw tmpfile(), add rename(), allow C++ build
    mingw_tmpfile (aliased to tmpfile()) has been quite a journey to
    implement as many different options kept failing on different systems.
    This version seems pretty stable now.

--- tools/common/polyfill.h
+++ tools/common/polyfill.h
@@ -13,16 +13,27 @@
 
 #ifdef __MINGW32__
 
+// NOTE: we include both stdio.h and cstdio because both of them will undef
+// tmpfile and redefine it to something else. Since we want to provide our
+// own implementation, we need to make sure both are included first, so that
+// we don't run the risk either of them is included after this file.
 #include <stdio.h>
+#ifdef __cplusplus
+#include <cstdio>
+#endif
 #include <stdlib.h>
 #include <errno.h>
 #include <stdint.h>
 #include <string.h>
+#include <process.h>
 #include <share.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <time.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 // if typedef doesn't exist (msvc, blah)
 typedef intptr_t ssize_t;
@@ -31,8 +42,8 @@ typedef intptr_t ssize_t;
 /* The original code is public domain -- Will Hartung 4/9/09 */
 /* Modifications, public domain as well, by Antti Haapala, 11/10/17
    - Switched to getc on 5/23/19 */
-
-ssize_t getline(char **lineptr, size_t *n, FILE *stream) {
+__attribute__((used))
+static ssize_t getline(char **lineptr, size_t *n, FILE *stream) {
     size_t pos;
     int c;
 
@@ -81,7 +92,8 @@ ssize_t getline(char **lineptr, size_t *n, FILE *stream) {
 }
 
 /* This function is original code in libdragon */
-char *strndup(const char *s, size_t n)
+__attribute__((used))
+static char *strndup(const char *s, size_t n)
 {
   size_t len = strnlen(s, n);
   char *ret = (char*)malloc(len + 1);
@@ -93,29 +105,88 @@ char *strndup(const char *s, size_t n)
 
 // tmpfile in mingw is broken (it uses msvcrt that tries to
 // create a file in C:\, which is non-writable nowadays)
+#ifdef tmpfile
+#undef tmpfile
+#endif
 #define tmpfile()   mingw_tmpfile()
 
-FILE *mingw_tmpfile(void) {
-    // We use the current directory for temporary files. Using GetTempFilePath is dangerous
-    // because a subprocess spawned without environment would receive C:\Windows which is not writable.
-    // So the cwd has a higher chance of actually working, for our use case of command line tools. 
-    char path[_MAX_PATH];
-    for (int i=0; i<4096; i++) {
-        // We use rand() which provides a 16-bit deterministic sequence. Again, for our use
-        // case is sufficient, given that _O_EXCL will make sure the file does not exist.
-        snprintf(path, sizeof(path), "mksprite-%04x", rand());
-        // This is taken from mingw's mkstemp implementation, adding _O_TEMPORARY
-        // to make the file autodelete
-        int fd = _sopen(path, _O_RDWR | _O_CREAT | _O_EXCL | _O_BINARY | _O_TEMPORARY, _SH_DENYNO, _S_IREAD | _S_IWRITE);
-        if (fd != -1)
+typedef void* HANDLE;
+typedef const char* LPCSTR;
+typedef int BOOL;
+#define INVALID_HANDLE_VALUE ((HANDLE)(long)-1)
+struct _SECURITY_ATTRIBUTES;
+
+// Access rights
+#define GENERIC_READ        0x80000000
+#define GENERIC_WRITE       0x40000000
+
+// Share modes
+#define FILE_SHARE_READ     0x00000001
+#define FILE_SHARE_WRITE    0x00000002
+#define FILE_SHARE_DELETE   0x00000004
+
+// Creation disposition
+#define CREATE_NEW          1
+
+// Flags and attributes
+#define FILE_ATTRIBUTE_TEMPORARY     0x00000100
+#define FILE_FLAG_DELETE_ON_CLOSE    0x04000000
+
+__declspec(dllimport) HANDLE __stdcall CreateFileA(
+    LPCSTR lpFileName,
+    unsigned long dwDesiredAccess,
+    unsigned long dwShareMode,
+    struct _SECURITY_ATTRIBUTES* lpSecurityAttributes,
+    unsigned long dwCreationDisposition,
+    unsigned long dwFlagsAndAttributes,
+    HANDLE hTemplateFile
+);
+__declspec(dllimport) int __stdcall CloseHandle(HANDLE);
+__declspec(dllimport) unsigned long __stdcall GetLastError(void);
+__declspec(dllimport) unsigned long __stdcall GetTickCount(void);
+
+__attribute__((used))
+static FILE *mingw_tmpfile(void) {
+    static int counter = 0;
+    char path[260];
+
+    for (int i = 0; i < 4096; i++) {
+        // Generate a random filename. Notice we *purposedly* not make this
+        // very random with PID, timestamp, etc. because this is the third
+        // iteration of mingw_tmpfile(): the previous ones were misbehaving
+        // in various ways on various CRTs, Windows versions, etc. 
+        // We want this code to be exercised often so that it is robust.
+        snprintf(path, sizeof(path), "mksprite-%04x.tmp", counter++);
+
+        HANDLE h = CreateFileA(
+            path,
+            GENERIC_READ | GENERIC_WRITE,
+            FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE,
+            NULL,
+            CREATE_NEW,
+            FILE_ATTRIBUTE_TEMPORARY | FILE_FLAG_DELETE_ON_CLOSE,
+            NULL
+        );
+
+        if (h != INVALID_HANDLE_VALUE) {
+            int fd = _open_osfhandle((intptr_t)h, _O_RDWR | _O_BINARY);
+            if (fd == -1) {
+                CloseHandle(h);
+                return NULL;
+            }
             return fdopen(fd, "w+b");
-        if (fd == -1 && errno != EEXIST)
-            return NULL;
+        }
+
+        // 80 = ERROR_FILE_EXISTS
+        if (GetLastError() != 80)
+            break;
     }
+
     return NULL;
 }
 
-char* strcasestr(const char* haystack, const char* needle)
+__attribute__((used))
+static char* strcasestr(const char* haystack, const char* needle)
 {
     size_t needle_len = strlen(needle);
     size_t haystack_len = strlen(haystack);
@@ -134,7 +205,8 @@ char* strcasestr(const char* haystack, const char* needle)
 }
 
 // Implementation from FreeBSD
-void *memmem(const void *l, size_t l_len, const void *s, size_t s_len)
+__attribute__((used))
+static void *memmem(const void *l, size_t l_len, const void *s, size_t s_len)
 {
 	char *cur, *last;
 	const char *cl = (const char *)l;
@@ -162,6 +234,44 @@ void *memmem(const void *l, size_t l_len, const void *s, size_t s_len)
 	return NULL;
 }
 
+// rename() that ovewrites the destination file
+#define rename(a,b)  mingw_rename(a,b)
+
+#define MOVEFILE_REPLACE_EXISTING 0x00000001
+#define MOVEFILE_WRITE_THROUGH    0x00000008
+
+__declspec(dllimport) int __stdcall MoveFileExA(const char *lpExistingFileName,
+                                                const char *lpNewFileName,
+                                                unsigned long dwFlags);
+__declspec(dllimport) unsigned long __stdcall GetLastError(void);
+
+static void map_windows_error_to_errno(unsigned long err) {
+    switch (err) {
+        case 2:    errno = ENOENT; break;        // ERROR_FILE_NOT_FOUND
+        case 3:    errno = ENOENT; break;        // ERROR_PATH_NOT_FOUND
+        case 5:    errno = EACCES; break;        // ERROR_ACCESS_DENIED
+        case 32:   errno = EBUSY; break;         // ERROR_SHARING_VIOLATION
+        case 80:   errno = EEXIST; break;        // ERROR_FILE_EXISTS
+        case 183:  errno = EEXIST; break;        // ERROR_ALREADY_EXISTS
+        default:   errno = EIO; break;           // Generic error
+    }
+}
+
+__attribute__((used))
+static int mingw_rename(const char *oldpath, const char *newpath) {
+    if (MoveFileExA(oldpath, newpath, MOVEFILE_REPLACE_EXISTING | MOVEFILE_WRITE_THROUGH)) {
+        return 0;
+    } else {
+        map_windows_error_to_errno(GetLastError());
+        return -1;
+    }
+}
+
+// POISX mkdir has a mode argument, but mingw's mkdir doesn't
+#define mkdir(path, mode) mkdir(path)
+
+#ifdef __cplusplus
+}
 #endif
 
 #endif
