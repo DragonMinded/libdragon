e1f11d6ca1a48450122af979fd7fe98bf4a30ba8
Giovanni Bajo <rasky@develer.com>
2025-09-26 17:06:12 +0200

    rdpq: add script to generate builtin fonts

---
+++ src/rdpq/mkfontbuiltin.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -euo pipefail
+
+# cd to the script directory
+cd "$(dirname "$0")"
+
+TMPDIR=/tmp
+FONTDB=../../examples/fontgallery/assets
+
+# Builtin fonts are monogram and at01, available in the fontgallery example
+BUILTIN="monogram at01"
+
+rm -f rdpq_font_builtin.c
+for font in $BUILTIN; do
+    "$N64_INST/bin/mkfont" --compress 0 --output $TMPDIR --range 20-BF --outline 1  "$@" "$FONTDB/$font.ttf"
+    OUTFN="$TMPDIR/$font.font64"
+    OUTFN_C=$(echo $OUTFN | sed 's/[/.]/_/g')
+    xxd -i $OUTFN | sed "s/${OUTFN_C}/__fontdb_${font}/g" >>rdpq_font_builtin.c
+done
