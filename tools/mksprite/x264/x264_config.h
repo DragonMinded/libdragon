9867423321c7971c23e1a3609856fc13d72723fb
Giovanni Bajo <giovannibajo@gmail.com>
2026-01-21 02:19:47 +0100

    Vendor x264

---
+++ tools/mksprite/x264/x264_config.h
@@ -0,0 +1,16 @@
+// Minimal x264_config.h for libdragon vendored amalgam build.
+#ifndef X264_CONFIG_H
+#define X264_CONFIG_H
+
+#define X264_GPL 1
+#define X264_INTERLACED 0
+#define X264_BIT_DEPTH 8
+#define X264_CHROMA_FORMAT 1
+
+#define X264_REV 0
+#define X264_REV_DIFF 0
+#define X264_VERSION "x264"
+#define X264_POINTVER "0.164"
+
+#endif
+
