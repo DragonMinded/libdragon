99ebe543494284b52914656a98b0f91a041a81d6
Matt 'Smartis' W <matt.weingaertner@gmail.com>
2026-01-26 17:29:44 +0100

    examples: fix warnings
    - Updating controllertest.c to latest joypad API
    - Replaced deprecated joypad identifier call with joybus API in microuiN64.c

--- examples/controllertest/controllertest.c
+++ examples/controllertest/controllertest.c
@@ -26,31 +26,31 @@ const char *format_accessory_type(int accessory_type)
     }
 }
 
-void print_inputs(_SI_condat *inputs)
+void print_inputs(joypad_inputs_t inputs)
 {
     printf(
         "Stick: %+04d,%+04d\n",
-        inputs->x, inputs->y
+        inputs.stick_x, inputs.stick_y
     );
     printf(
         "D-U:%d D-D:%d D-L:%d D-R:%d C-U:%d C-D:%d C-L:%d C-R:%d\n",
-        inputs->up, inputs->down,
-        inputs->left, inputs->right,
-        inputs->C_up, inputs->C_down,
-        inputs->C_left, inputs->C_right
+        inputs.btn.d_up, inputs.btn.d_down,
+        inputs.btn.d_left, inputs.btn.d_right,
+        inputs.btn.c_up, inputs.btn.c_down,
+        inputs.btn.c_left, inputs.btn.c_right
     );
     printf(
         "A:%d B:%d L:%d R:%d Z:%d Start:%d\n",
-        inputs->A, inputs->B,
-        inputs->L, inputs->R,
-        inputs->Z, inputs->start
+        inputs.btn.a, inputs.btn.b,
+        inputs.btn.l, inputs.btn.r,
+        inputs.btn.z, inputs.btn.start
     );
 }
 
 int main(void)
 {
     timer_init();
-    controller_init();
+    joypad_init();
     debug_init_isviewer();
     console_init();
     console_set_render_mode(RENDER_MANUAL);
@@ -62,17 +62,20 @@ int main(void)
 
         printf("LibDragon Controller Subsystem Test\n\n");
 
-        controller_scan();
-        struct controller_data inputs = get_keys_pressed();
+        joypad_poll();
 
         for (int port = 0; port < 4; port++)
         {
-            int accessory_type = identify_accessory(port);
+            if (!joypad_is_connected(port)) continue;
+
+            joypad_inputs_t in = joypad_get_inputs(port);
+
+            int accessory_type = joypad_get_accessory_type(port);
 
             printf("Port %d ", port + 1);
             printf("Accessory: %s ", format_accessory_type(accessory_type));
             printf("\n");
-            print_inputs(&inputs.c[port]);
+            print_inputs(in);
             printf("\n");
         }
 
