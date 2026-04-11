4d38310fcbb5c2547d1ef6941f389877e25b9b6a
Dragorn421 <Dragorn421@users.noreply.github.com>
2026-04-11 08:51:08 +0200

    RDP validator: warn on combined rgb passed to mul with (potential) overflow

--- include/rdpq_macros.h
+++ include/rdpq_macros.h
@@ -914,7 +914,7 @@ typedef uint32_t rdpq_blender_t;
  */
 #define ZBUF_MAX        0xFFFC 
 
-#ifndef __ASSEMBLER__
+#if !defined(__ASSEMBLER__) && defined(N64)
 
 ///@cond
 extern uint16_t __rdpq_zfp14(float f);
