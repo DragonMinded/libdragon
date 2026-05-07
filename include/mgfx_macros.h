5a2edcf75fec9316395abbb08f9345fbe33f3b03
Dennis Heinze <dennis.heinze@mailbox.org>
2026-05-07 19:10:30 +0200

    improve docs

--- include/mgfx_macros.h
+++ include/mgfx_macros.h
@@ -12,14 +12,14 @@
 /** @brief Convert a number to s10.5 format, which is used for vertex positions in mgfx. */
 #define MGFX_S10_5(v)       ((v)*(1<<MGFX_VTX_POS_SHIFT))
 
-/** @brief Convert a number to s8.8 format, which may be used for texture coordinates in mgfx (Though any signed fixed point format is accepted). */
-#define MGFX_S8_8(v)        ((v)*(1<<MGFX_VTX_TEX_SHIFT))
+/** @brief Convert a number to s7.8 format, which may be used for texture coordinates in mgfx (Though any signed fixed point format is accepted). */
+#define MGFX_S7_8(v)        ((v)*(1<<MGFX_VTX_TEX_SHIFT))
 
 /** @brief Construct a vertex position in a format supported by the mgfx shader. */
 #define MGFX_POS(x, y, z)   { MGFX_S10_5(x), MGFX_S10_5(y), MGFX_S10_5(z) }
 
 /** @brief Construct a texture coordinate in a format supported by the mgfx shader. */
-#define MGFX_TEX(s, t)      { MGFX_S8_8(s), MGFX_S8_8(t) }
+#define MGFX_TEX(s, t)      { MGFX_S7_8(s), MGFX_S7_8(t) }
 
 /** @brief Construct a vertex normal in a format supported by the mgfx shader. */
 #define MGFX_NRM(x, y, z)   ((((x) & 0x1F)<<11) | \
