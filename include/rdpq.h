5cb976aab11eb30622c33b112c120a1107eedb5e
Dragorn421 <Dragorn421@users.noreply.github.com>
2026-05-12 16:45:53 +0200

    revert workarounds in rdpq_set_tile_size_fx and ROUND_DOWN

--- include/rdpq.h
+++ include/rdpq.h
@@ -680,6 +680,7 @@ inline void rdpq_load_tlut_raw(rdpq_tile_t tile, int color_idx, int num_colors)
  */
 inline void rdpq_set_tile_size_fx(rdpq_tile_t tile, uint16_t s0, uint16_t t0, uint16_t s1, uint16_t t1)
 {
+    assertf((s0) >= 0 && (t0) >= 0 && (s1) >= 0 && (t1) >= 0, "texture coordinates must be positive");
     assertf((s0) <= 1024*4 && (t0) <= 1024*4 && (s1) <= 1024*4 && (t1) <= 1024*4, "texture coordinates must be smaller than 1024");
 
     extern void __rdpq_write8_syncchange(uint32_t, uint32_t, uint32_t, uint32_t);
